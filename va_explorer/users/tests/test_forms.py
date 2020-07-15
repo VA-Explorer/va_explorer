0948b95c76f53b070e7f1bfc588899a1d425d0fa
Diana Eastman <deastman@mitre.org>
2020-07-15 12:02:57 -0400

    Add more tests for UserCreationForm.

--- va_explorer/users/tests/test_forms.py
+++ va_explorer/users/tests/test_forms.py
@@ -8,6 +8,21 @@ pytestmark = pytest.mark.django_db
 
 class TestUserCreationForm:
     def test_email_uniqueness(self):
+        # A user with existing_user params exists already.
+        existing_user = UserFactory.create()
+
+        form = ExtendedUserCreationForm(
+            {
+                "first_name": existing_user.first_name,
+                "last_name": existing_user.last_name,
+                "email": existing_user.email,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "email" in form.errors
+
         # A user with proto_user params does not exist yet.
         proto_user = UserFactory.build()
 
@@ -21,14 +36,29 @@ class TestUserCreationForm:
 
         assert form.is_valid()
 
-        # Creating a user.
-        form.save()
+    def test_email_required(self):
+        # A user with proto_user params does not exist yet.
+        proto_user = UserFactory.build()
 
-        # The user with proto_user params already exists,
-        # hence cannot be created.
         form = ExtendedUserCreationForm(
             {
                 "first_name": proto_user.first_name,
+                "last_name": proto_user.last_name,
+                "email": "",
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "email" in form.errors
+
+    def first_last_name_required(self):
+        # A user with proto_user params does not exist yet.
+        proto_user = UserFactory.build()
+
+        form = ExtendedUserCreationForm(
+            {
+                "first_name": "",
                 "last_name": proto_user.last_name,
                 "email": proto_user.email,
             }
@@ -36,4 +66,20 @@ class TestUserCreationForm:
 
         assert not form.is_valid()
         assert len(form.errors) == 1
-        assert "email" in form.errors
+        assert "first_name" in form.errors
+
+    def test_last_name_required(self):
+        # A user with proto_user params does not exist yet.
+        proto_user = UserFactory.build()
+
+        form = ExtendedUserCreationForm(
+            {
+                "first_name": proto_user.first_name,
+                "last_name": "",
+                "email": proto_user.email,
+            }
+        )
+
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "last_name" in form.errors
