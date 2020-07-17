35206e3f73e40c10d0c9c59017ce35ade8f69d79
Diana Eastman <deastman@mitre.org>
2020-07-17 08:34:03 -0400

    Use the django email backend for sending emails in the dev environment (emails are printed to the console.

--- config/settings/local.py
+++ config/settings/local.py
@@ -26,9 +26,11 @@ CACHES = {
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+# EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = 1025
+# EMAIL_PORT = 1025
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
