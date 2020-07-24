8c5a755769242c44e8176ef5b49d6ebd3f06f0c2
Diana Eastman <deastman@mitre.org>
2020-07-24 12:09:58 -0400

    Add custom mixin to check if user authorized and has valid_password.

--- va_explorer/users/views.py
+++ va_explorer/users/views.py
@@ -1,21 +1,23 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     CreateView,
     DetailView,
+    FormView,
     ListView,
     RedirectView,
     UpdateView,
 )
 
-from .forms import ExtendedUserCreationForm
+from ..utils.mixins import CustomAuthMixin
+from .forms import ExtendedUserCreationForm, UserSetPasswordForm, UserUpdateForm
 
 User = get_user_model()
 
 
-class UserIndexView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+class UserIndexView(CustomAuthMixin, PermissionRequiredMixin, ListView):
     # https://github.com/pennersr/django-allauth/blob/c19a212c6ee786af1bb8bc1b07eb2aa8e2bf531b/allauth/account/urls.py
     login_url = reverse_lazy("account_login")
     permission_required = "users.view_user"
@@ -26,7 +28,7 @@ user_index_view = UserIndexView.as_view()
 
 
 class UserCreateView(
-    LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, CreateView
+    CustomAuthMixin, PermissionRequiredMixin, SuccessMessageMixin, CreateView
 ):
     login_url = reverse_lazy("account_login")
     permission_required = "users.add_user"
@@ -46,7 +48,7 @@ class UserCreateView(
 user_create_view = UserCreateView.as_view()
 
 
-class UserDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+class UserDetailView(CustomAuthMixin, PermissionRequiredMixin, DetailView):
     login_url = reverse_lazy("account_login")
     permission_required = "users.view_user"
     model = User
@@ -56,19 +58,12 @@ user_detail_view = UserDetailView.as_view()
 
 
 class UserUpdateView(
-    LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView
+    CustomAuthMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView
 ):
     login_url = reverse_lazy("account_login")
     permission_required = "users.change_user"
-    model = User
-    fields = [
-        "first_name",
-        "last_name",
-        "email",
-        "is_superuser",
-        "is_active",
-        "groups",
-    ]
+    form_class = UserUpdateForm
+    template_name = "users/user_update.html"
     success_message = "User successfully updated!"
 
     def get_success_url(self):
@@ -80,6 +75,12 @@ class UserUpdateView(
     def form_valid(self, form):
         return super().form_valid(form)
 
+    # TODO: Remove if we do not require email confirmation; we will no longer need the lines below
+    # def get_form_kwargs(self):
+    #     kw = super(UserUpdateView, self).get_form_kwargs()
+    #     kw["request"] = self.request  # the trick!
+    #     return kw
+
 
 user_update_view = UserUpdateView.as_view()
 
@@ -89,7 +90,27 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"pk": self.request.user.id})
+        if self.request.user.has_valid_password:
+            return reverse("users:detail", kwargs={"pk": self.request.user.id})
+        return reverse_lazy("users:set_password")
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+
+class UserSetPasswordView(FormView, LoginRequiredMixin, SuccessMessageMixin):
+    login_url = reverse_lazy("account_login")
+    form_class = UserSetPasswordForm
+    template_name = "users/user_set_password.html"
+    success_message = "User successfully set password!"
+    success_url = "/about"
+
+    def form_valid(self, form):
+        form.save(self.request.user)
+        # See django docs:
+        # https://docs.djangoproject.com/en/dev/topics/auth/default/#django.contrib.auth.update_session_auth_hash
+        update_session_auth_hash(self.request, self.request.user)
+        return super().form_valid(form)
+
+
+user_set_password_view = UserSetPasswordView.as_view()
