acba21f4ecc819e817db46a6a08ecf692d10e4cf
Diana Eastman <deastman@mitre.org>
2020-08-17 20:29:05 -0400

    Remove print statements.

--- va_explorer/users/views.py
+++ va_explorer/users/views.py
@@ -41,8 +41,6 @@ class UserCreateView(
 
     def get_form_kwargs(self):
         kw = super(UserCreateView, self).get_form_kwargs()
-        print("form kwargs")
-        print(kw)
         kw["request"] = self.request  # the trick!
         return kw
 
