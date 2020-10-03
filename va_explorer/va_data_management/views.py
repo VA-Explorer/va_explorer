c564aeccde31b50eb5700fd3c929bc03fea70a64
Pete K <krautscheid@mitre.org>
2020-10-02 22:52:47 -0400

    WIP: Scratch display of all VAs in system along with codings and issues

--- va_explorer/va_data_management/views.py
+++ va_explorer/va_data_management/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import VerbalAutopsy, CauseOfDeath, CauseCodingIssue
 
-# Create your views here.
+def index(request):
+    va_list = VerbalAutopsy.objects.prefetch_related("causes", "coding_issues")
+    context = { "va_list": va_list }
+    return render(request, "va_data_management/index.html", context)
