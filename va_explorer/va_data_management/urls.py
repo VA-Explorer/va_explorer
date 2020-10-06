00f50d5057711dcfbc444fb34a5ccb0e7bdcbf06
Pete K <krautscheid@mitre.org>
2020-10-05 22:23:50 -0400

    WIP: Basic capability to view VA data and edit based on errors

--- va_explorer/va_data_management/urls.py
+++ va_explorer/va_data_management/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "data_management"
 
 urlpatterns = [
-    path("", view=views.index, name="index")
+    path("", view=views.index, name="index"),
+    path("show/<int:id>", view=views.show, name="show"),
+    path("edit/<int:id>", view=views.edit, name="edit"),
+    path("save/<int:id>", view=views.save, name="save")
 ]
--- va_explorer/va_data_management/views.py
+++ va_explorer/va_data_management/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
-from .models import VerbalAutopsy, CauseOfDeath, CauseCodingIssue
+from django.shortcuts import render, redirect
+from .models import VerbalAutopsy, CauseOfDeath, CauseCodingIssue, Location
+from .forms import VerbalAutopsyForm
 
 def index(request):
-    va_list = VerbalAutopsy.objects.prefetch_related("causes", "coding_issues")
-    context = { "va_list": va_list }
+    va_list = VerbalAutopsy.objects.prefetch_related("causes", "coding_issues").order_by("id")
+    va_data = [{
+        "id": va.id,
+        "name": va.Id10007,
+        "facility": va.location.name,
+        "cause": va.causes.first().cause if va.causes.first() else "",
+        "warnings": len([issue for issue in va.coding_issues.all() if issue.severity == 'warning']),
+        "errors": len([issue for issue in va.coding_issues.all() if issue.severity == 'error'])
+    } for va in va_list]
+    context = { "va_data": va_data }
     return render(request, "va_data_management/index.html", context)
+
+# TODO: Use standard django CRUD conventions; e.g. see https://stackoverflow.com/questions/4673985/how-to-update-an-object-from-edit-form-in-django
+
+# TODO: Is the convention to use pk instead of id in django?
+def show(request, id):
+    # TODO: The use of values() to get a dictionary is probably not ideal
+    # and is perhaps more fragile than va = VerbalAutopsy.objects.get(id=id)
+    va = VerbalAutopsy.objects.filter(id=id).values()[0]
+    del va["id"]
+    del va["location_id"]
+    coding_issues = CauseCodingIssue.objects.filter(verbalautopsy_id=id)
+    warnings = [issue for issue in coding_issues if issue.severity == 'warning']
+    errors = [issue for issue in coding_issues if issue.severity == 'error']
+    context = { "id": id, "va": va, "warnings": warnings, "errors": errors }
+    return render(request, "va_data_management/show.html", context)
+
+def edit(request, id):
+    # TODO: The use of values() to get a dictionary is probably not ideal
+    # and is perhaps more fragile than va = VerbalAutopsy.objects.get(id=id)
+    # TODO: We only want to allow editing of some fields
+    # TODO: We want to provide context for all the fields
+    # TODO: We probably want to use a django Form here or crispy forms
+    va = VerbalAutopsy.objects.filter(id=id).values()[0]
+    del va["id"]
+    del va["location_id"]
+    context = { "id": id, "va": va }
+    return render(request, "va_data_management/edit.html", context)
+
+def save(request, id):
+    # TODO: There are probably more appropriate ways to handle form editing in django
+    va = VerbalAutopsy.objects.get(id=id)
+    form = VerbalAutopsyForm(request.POST, instance=va)
+    if form.is_valid():
+        form.save()
+    return redirect('va_data_management:show', id=id)
