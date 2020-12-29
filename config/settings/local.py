430acce4ef457062b45da0de5409c553f3f1eca4
Tim Shaffer <tshaffer@mitre.org>
2020-12-29 11:43:29 -0500

    Update IP list.

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,8 +14,7 @@ SECRET_KEY = env(
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
-ip_list = socket.gethostbyname_ex(socket.gethostname())[2]
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"] + ip_list + [".".join(ip.split(".")[:-1] + ["1"]) for ip in ip_list]
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 # Celery
 
