1077864cd99009562bb075019fb9286a78f55b4f
Diana Eastman <deastman@mitre.org>
2021-02-09 20:38:10 -0500

    Add missing import and merge conflicting migrations on User.

--- va_explorer/users/models.py
+++ va_explorer/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import ManyToManyField
 from django.urls import reverse
 from django.utils.timezone import localtime
 from django.utils.translation import gettext_lazy as _
