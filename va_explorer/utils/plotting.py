bdfa295bda9b0acf34049dec9b5767d1e0364291
Adam Barnes <ajbarnes@mitre.org>
2021-03-03 15:27:34 -0500

    Apply import sort + formatting to dashboard code. Update pandas to handle pytest warnings.

--- va_explorer/utils/plotting.py
+++ va_explorer/utils/plotting.py
@@ -5,18 +5,19 @@ Created on Wed Feb 10 23:56:26 2021
 
 @author: babraham
 """
-import pandas as pd
-import plotly.graph_objs as go
-import plotly.figure_factory as ff
-import plotly.express as px
 import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
 # ===========PLOTTING PROPERTIES/VARIABLES=====================#
 D3 = px.colors.qualitative.D3
 PLOTLY = px.colors.qualitative.Plotly
+
+
 # plotting properties defined below
-#======= LOTTING PROPERTIES ==============#
+# ======= LOTTING PROPERTIES ==============#
 def load_lookup_dicts():
     lookup = dict()
     # dictionary mapping time labels to days (or all)
@@ -39,28 +40,29 @@ def load_lookup_dicts():
     ]
     # colorscale used for map
     lookup["colorscales"] = {
-            "primary": [(0.0, 'rgb(255,255,255)'),
-                 (1e-20, 'rgb(0, 147, 146)'),
-                 (0.167, 'rgb(0, 147, 146)'),
-                 (0.167, 'rgb(57, 177, 133)'),
-                 (0.333, 'rgb(57, 177, 133)'),
-                 (0.333, 'rgb(156, 203, 134)'),
-                 (0.5, 'rgb(156, 203, 134)'),
-                 (0.5, 'rgb(233, 226, 156)'),
-                 (0.667, 'rgb(233, 226, 156)'),
-                 (0.667, 'rgb(238, 180, 121)'),
-                 (0.833, 'rgb(238, 180, 121)'),
-                 (0.833, 'rgb(232, 132, 113)'),
-                 (1.0, 'rgb(232, 132, 113)')],
-            "secondary": [(0.0, 'rgb(255,255,255)'),
-                  (0.001, 'rgb(230,230,230)'),
-                  (1.0, 'rgb(230,230,230)')]
-        }
-            
-    lookup["line_colors"] = {
-            "primary": "black", 
-            "secondary": "gray"
+        "primary": [
+            (0.0, "rgb(255,255,255)"),
+            (1e-20, "rgb(0, 147, 146)"),
+            (0.167, "rgb(0, 147, 146)"),
+            (0.167, "rgb(57, 177, 133)"),
+            (0.333, "rgb(57, 177, 133)"),
+            (0.333, "rgb(156, 203, 134)"),
+            (0.5, "rgb(156, 203, 134)"),
+            (0.5, "rgb(233, 226, 156)"),
+            (0.667, "rgb(233, 226, 156)"),
+            (0.667, "rgb(238, 180, 121)"),
+            (0.833, "rgb(238, 180, 121)"),
+            (0.833, "rgb(232, 132, 113)"),
+            (1.0, "rgb(232, 132, 113)"),
+        ],
+        "secondary": [
+            (0.0, "rgb(255,255,255)"),
+            (0.001, "rgb(230,230,230)"),
+            (1.0, "rgb(230,230,230)"),
+        ],
     }
+
+    lookup["line_colors"] = {"primary": "black", "secondary": "gray"}
     # dictionary mapping raw map metrics to human-readable names
     lookup["metric_names"] = {
         "all": "All Causes",
@@ -76,13 +78,13 @@ def load_lookup_dicts():
         "Liver cirrhosis": "Liver Cirrhosis",
         "Digestive neoplasms": "Digestive Neoplasm",
         "Other and unspecified infect dis": "Other",
-    }    
+    }
     # dictionary mapping place of death names to more human-readable names
     lookup["death_location_names"] = {
         "on_route_to_hospital_or_facility": "En Route to Facility",
         "DK": "Unknown",
         "other_health_facility": "Other Health Facility",
-    }    
+    }
     # formats for montly, weekly, and yearly dates
     lookup["date_display_formats"] = {
         "week": "%d/%m/%Y",
@@ -92,88 +94,129 @@ def load_lookup_dicts():
 
     return lookup
 
+
 LOOKUP = load_lookup_dicts()
 
- 
-# get counts of a categorical field from va data. Field name can either be a 
-# column name in the dataframe or a demographic lookup key. Change final column name with display_name argument. 
+
+# get counts of a categorical field from va data. Field name can either be a
+# column name in the dataframe or a demographic lookup key. Change final column name with display_name argument.
 def get_field_counts(va_df, field_name, full_labels=False, display_name=None):
     if field_name not in va_df.columns:
-        # if no matching column in va_df for field name, try lookup in the demo_to_col dict. 
+        # if no matching column in va_df for field name, try lookup in the demo_to_col dict.
         if not display_name:
             # if no display name provided, use original key
             display_name = field_name
-        field_name = LOOKUP['demo_to_col'].get(field_name, field_name)
+        field_name = LOOKUP["demo_to_col"].get(field_name, field_name)
     assert field_name in va_df.columns
-    
-    va_df = (va_df[field_name].value_counts()
-              .reset_index()
-              .assign(index = lambda df: df['index'].str.capitalize())
-              .rename(columns={field_name: 'count', 'index': 'group'})
-              .assign(percent = lambda df: df.apply(lambda x: np.round(100*x['count']/df['count'].sum(),1), axis=1))
-              .assign(label = lambda df: df['percent'].astype(str) + '%')
+
+    va_df = (
+        va_df[field_name]
+        .value_counts()
+        .reset_index()
+        .assign(index=lambda df: df["index"].str.capitalize())
+        .rename(columns={field_name: "count", "index": "group"})
+        .assign(
+            percent=lambda df: df.apply(
+                lambda x: np.round(100 * x["count"] / df["count"].sum(), 1), axis=1
+            )
+        )
+        .assign(label=lambda df: df["percent"].astype(str) + "%")
     )
     if display_name:
-        va_df = va_df.rename(columns={'group': display_name})
-        
+        va_df = va_df.rename(columns={"group": display_name})
+
     if full_labels:
-        va_df['label'] = va_df['count'].astype(str) + '<br> (' + va_df['percent'].astype(str) + '%)'
+        va_df["label"] = (
+            va_df["count"].astype(str) + "<br> (" + va_df["percent"].astype(str) + "%)"
+        )
 
     return va_df
 
-#===========DEMOGRAPHIC PLOT LOGIC=========================#
+
+# ===========DEMOGRAPHIC PLOT LOGIC=========================#
 # create a multiplot of va counts by gender, age, and place of death
 def demographic_plot(va_df, no_grids=True, column_widths=None, height=600, title=None):
     if not column_widths:
-        first_width = .4
+        first_width = 0.4
         column_widths = [first_width, 1 - first_width]
     comb_fig = make_subplots(
-        rows=2, cols=2,
-        specs=[[{'type': 'bar'}, {'type': 'bar'}],
-               [{"colspan": 2}, None]],
-        subplot_titles=("Gender","Age Group", "Place of Death"), 
-        column_widths=column_widths, 
-        vertical_spacing=.15)
+        rows=2,
+        cols=2,
+        specs=[[{"type": "bar"}, {"type": "bar"}], [{"colspan": 2}, None]],
+        subplot_titles=("Gender", "Age Group", "Place of Death"),
+        column_widths=column_widths,
+        vertical_spacing=0.15,
+    )
 
     # gender
-    sex_df = get_field_counts(va_df, 'sex', display_name='gender')
-    comb_fig.add_trace(go.Bar(name='Gender', x=sex_df['gender'], y=sex_df['count'], text=sex_df['label'], textposition='auto',
-               showlegend=False, marker_color=PLOTLY),
-        row=1, col=1)
+    sex_df = get_field_counts(va_df, "sex", display_name="gender")
+    comb_fig.add_trace(
+        go.Bar(
+            name="Gender",
+            x=sex_df["gender"],
+            y=sex_df["count"],
+            text=sex_df["label"],
+            textposition="auto",
+            showlegend=False,
+            marker_color=PLOTLY,
+        ),
+        row=1,
+        col=1,
+    )
 
     # age groups
-    age_df = get_field_counts(va_df, 'age group', display_name='age_group')
-    comb_fig.add_trace(go.Bar(name='Age Group', x=age_df['age_group'], y=age_df['count'], text=age_df['label'], textposition='auto',
-               showlegend=False, marker_color=D3),
-        row=1, col=2,)
+    age_df = get_field_counts(va_df, "age group", display_name="age_group")
+    comb_fig.add_trace(
+        go.Bar(
+            name="Age Group",
+            x=age_df["age_group"],
+            y=age_df["count"],
+            text=age_df["label"],
+            textposition="auto",
+            showlegend=False,
+            marker_color=D3,
+        ),
+        row=1,
+        col=2,
+    )
 
     # place of death
-    loc_cts = get_field_counts(va_df, 'place of death', display_name='location')
-    lookup = LOOKUP['death_location_names']
-    loc_cts['location'] = loc_cts['location'].apply(lambda x: lookup.get(x.lower(), x.capitalize()))
+    loc_cts = get_field_counts(va_df, "place of death", display_name="location")
+    lookup = LOOKUP["death_location_names"]
+    loc_cts["location"] = loc_cts["location"].apply(
+        lambda x: lookup.get(x.lower(), x.capitalize())
+    )
     comb_fig.add_trace(
-        go.Bar(name='Place of Death', 
-               x=loc_cts['count'], y=loc_cts['location'], orientation='h', showlegend=False,
-               text=loc_cts['label'],
-            textposition="auto", marker_color=D3[4]),
-        row=2, col=1
+        go.Bar(
+            name="Place of Death",
+            x=loc_cts["count"],
+            y=loc_cts["location"],
+            orientation="h",
+            showlegend=False,
+            text=loc_cts["label"],
+            textposition="auto",
+            marker_color=D3[4],
+        ),
+        row=2,
+        col=1,
     )
     if no_grids:
         comb_fig.update_xaxes(showgrid=False)
         comb_fig.update_yaxes(showgrid=False)
-        
+
     if title:
         comb_fig.update_layout(title_text=title)
-        
+
     return comb_fig.update_layout(height=height)
 
-#===========CAUSE OF DEATH PLOT LOGIC=========================#
+
+# ===========CAUSE OF DEATH PLOT LOGIC=========================#
 # plot top N causes of death in va_data either overall or by factor/demographic
 def cause_of_death_plot(va_df, factor, N=10, chosen_cod="all"):
     figure = go.Figure()
     factor = factor.lower()
     plot_fn = go.Bar
-    
+
     if factor != "all":
         assert factor in ["age group", "sex", "place of death"]
         factor_col = LOOKUP["demo_to_col"][factor]
@@ -193,30 +236,42 @@ def cause_of_death_plot(va_df, factor, N=10, chosen_cod="all"):
     # make index labels pretty
     counts.index = [LOOKUP["metric_names"].get(x, x) for x in counts.index]
     counts["cod"] = counts.index
-    counts = (counts[counts["cod"] != "All"].sort_values(by="All", ascending=False).head(N))
+    counts = (
+        counts[counts["cod"] != "All"].sort_values(by="All", ascending=False).head(N)
+    )
     groups = list(set(counts.columns).difference(set(["cod"])))
-    
-    lines = [LOOKUP["line_colors"]["secondary"] for j in range(len(counts['cod']))]
-    widths = np.repeat(1, len(counts['cod']))
+
+    lines = [LOOKUP["line_colors"]["secondary"] for j in range(len(counts["cod"]))]
+    widths = np.repeat(1, len(counts["cod"]))
     if chosen_cod != "all":
         chosen_cod = LOOKUP["metric_names"].get(chosen_cod, chosen_cod.capitalize())
         if chosen_cod in counts.index:
             chosen_idx = counts.index.get_loc(chosen_cod)
-            lines[chosen_idx] = "#e0b816" 
+            lines[chosen_idx] = "#e0b816"
             widths[chosen_idx] = 4
             counts["cod"][chosen_idx] = "<b>" + counts["cod"][chosen_idx] + "</b>"
 
     if factor != "all":
         groups.remove("All")
     for i, group in enumerate(groups):
-        if factor == "all": 
+        if factor == "all":
             # calculate percent as % of all cods (column-wise calculation)
-            counts[f"{group}_pct"] = np.round(100 * counts[group] / counts[group].sum(), 1)
+            counts[f"{group}_pct"] = np.round(
+                100 * counts[group] / counts[group].sum(), 1
+            )
         else:
             # calculate percent as % across groups for specific cod (row-wise calculation)
-            counts[f"{group}_pct"] = counts.apply(lambda row: np.round(100 * row[group] / row[groups].sum(), 1), axis=1)
-        counts["text"] = f"<i>{group.capitalize()}</i>: " + counts[group].astype(str) + " (" + counts[f"{group}_pct"].astype(str) + " %)"
-        
+            counts[f"{group}_pct"] = counts.apply(
+                lambda row: np.round(100 * row[group] / row[groups].sum(), 1), axis=1
+            )
+        counts["text"] = (
+            f"<i>{group.capitalize()}</i>: "
+            + counts[group].astype(str)
+            + " ("
+            + counts[f"{group}_pct"].astype(str)
+            + " %)"
+        )
+
         # counts
         figure.add_trace(
             plot_fn(
@@ -225,10 +280,9 @@ def cause_of_death_plot(va_df, factor, N=10, chosen_cod="all"):
                 text=counts["text"],
                 name=group.capitalize(),
                 orientation="v",
-                hovertemplate = "<b>%{x}</b><br>%{text}<extra></extra>",
+                hovertemplate="<b>%{x}</b><br>%{text}<extra></extra>",
                 marker=dict(
-                    color= LOOKUP["color_list"][i],
-                    line=dict(color=lines, width=widths),
+                    color=LOOKUP["color_list"][i], line=dict(color=lines, width=widths),
                 ),
             )
         )
@@ -238,13 +292,12 @@ def cause_of_death_plot(va_df, factor, N=10, chosen_cod="all"):
                 y=counts[f"{group}_pct"],
                 x=counts["cod"],
                 text=counts["text"],
-                hovertemplate = "<b>%{x}</b> <br> %{text}<extra></extra>",
+                hovertemplate="<b>%{x}</b> <br> %{text}<extra></extra>",
                 name=group.capitalize(),
                 orientation="v",
                 visible=False,
                 marker=dict(
-                    color= LOOKUP["color_list"][i],
-                    line=dict(color=lines, width=widths),
+                    color=LOOKUP["color_list"][i], line=dict(color=lines, width=widths),
                 ),
             )
         )
@@ -254,30 +307,40 @@ def cause_of_death_plot(va_df, factor, N=10, chosen_cod="all"):
         xaxis_tickangle=-45,
         yaxis_title="Count",
         updatemenus=[
-        dict(
-            type="buttons",
-            direction="right",
-            x = 1,
-            y=1.2,
-            active=0,
-            buttons=list([
-                dict(label="Counts",
-                     method="update",
-                     args=[{"visible": [True, False] * len(groups)},
-                           {"yaxis": {"title": "Count"}}
-                          ]),
-                dict(label="Percents",
-                     method="update",
-                     args = [{"visible": [False, True] * len(groups)},
-                             {'yaxis': {'title': '% of Total'}}
-                            ]),
-  
-            ]),
-        )])
-    
+            dict(
+                type="buttons",
+                direction="right",
+                x=1,
+                y=1.2,
+                active=0,
+                buttons=list(
+                    [
+                        dict(
+                            label="Counts",
+                            method="update",
+                            args=[
+                                {"visible": [True, False] * len(groups)},
+                                {"yaxis": {"title": "Count"}},
+                            ],
+                        ),
+                        dict(
+                            label="Percents",
+                            method="update",
+                            args=[
+                                {"visible": [False, True] * len(groups)},
+                                {"yaxis": {"title": "% of Total"}},
+                            ],
+                        ),
+                    ]
+                ),
+            )
+        ],
+    )
+
     return figure
 
-#========TREND/TIMESERIES PLOT LOGI======================#
+
+# ========TREND/TIMESERIES PLOT LOGI======================#
 def va_trend_plot(va_df, group_period, factor="All", title=None):
     figure = go.Figure()
     group_period = group_period.lower()
@@ -286,9 +349,7 @@ def va_trend_plot(va_df, group_period, factor="All", title=None):
     va_df["timegroup"] = pd.to_datetime(va_df["date"])
     if group_period == "week":
         va_df["timegroup"] = pd.to_datetime(
-            va_df["date"]
-            .dt.to_period("W")
-            .apply(lambda x: x.strftime("%Y-%m-%d"))
+            va_df["date"].dt.to_period("W").apply(lambda x: x.strftime("%Y-%m-%d"))
         )
     elif group_period == "month":
         va_df["timegroup"] = pd.to_datetime(
@@ -296,9 +357,9 @@ def va_trend_plot(va_df, group_period, factor="All", title=None):
         )
     elif group_period == "year":
         va_df["timegroup"] = va_df["date"].dt.to_period("Y").astype(str)
-    
+
     dtype = "category" if group_period == "year" else "date"
-    
+
     factor = factor.lower()
     if factor != "all":
         assert factor in LOOKUP["demo_to_col"]
@@ -320,7 +381,7 @@ def va_trend_plot(va_df, group_period, factor="All", title=None):
             .rename(columns={"id": "all"})
         )
         plot_fn = go.Bar
-    
+
     for i, group in enumerate(trend_counts.columns.tolist()):
         figure.add_trace(
             plot_fn(
