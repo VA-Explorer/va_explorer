72139460157c314f07b441afb58a20e34eb6e7ed
QollinsOchieng <qollinsochieng@gmail.com>
2021-03-03 14:34:25 +0300

    updated GUI pushing to dhis and syncing with dhis2status

--- va_explorer/dhis_manager/views.py
+++ va_explorer/dhis_manager/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from va_explorer.va_data_management.management.commands.run_dhis import Command
 from va_explorer.va_data_management.models import dhisStatus,VerbalAutopsy
+import environ
 # Create your views here.
 
 @login_required
@@ -9,15 +10,31 @@ def index(request):
     # to subset few rows,add at the end [:10] for 10 rows etc..
     # exclude vas that have no dhis2 status; not pushed
     push = "notyet"
-    vadata = VerbalAutopsy.objects.filter(causes__isnull=False,dhisva__isnull=True).count()
-    valist = VerbalAutopsy.objects.filter(causes__isnull=False, dhisva__isnull=True)
-    list1 = list()
-    for va in valist:
-        list1.append(va.id)
+    env = environ.Env()
+    # DHIS2 VARIABLES
+    DHIS2_USER = env("DHIS2_USER")
+    DHIS2_PASS = env("DHIS2_PASS")
+
+    cmd = Command()
+    auth = (DHIS2_USER,DHIS2_PASS)
+    eventsnum = cmd.getEventsValues('sv91bCroFFx', auth)
+    cmd.syncDHISStatus()
+    events = cmd.getPushedVA('sv91bCroFFx', auth)
+
+    valist = VerbalAutopsy.objects.filter(causes__isnull=False, dhisva__isnull=True).values_list('id',flat=True)
+    list1 = list(valist)
+    list1 = [str(i) for i in list1]
+
+    list3 = list()
+    for x in list1:
+        if x not in events:
+            list3.append(x)
+
+    #vadata = VerbalAutopsy.objects.filter(causes__isnull=False, dhisva__isnull=True).count()
 
     context = {
-        "vanum":str(vadata),
-        "valist":list1,
+        "vanum":str(len(list3)),
+        "total":str(eventsnum),
         "push":push
     }
     return render(request,"pages/dhis.html",context)
@@ -25,16 +42,37 @@ def index(request):
 
 @login_required
 def pushDHIS(request):
-    cl = Command()
-    numPushed,numTotal,status = cl.handle()
+
+    env = environ.Env()
+    # DHIS2 VARIABLES
+    DHIS2_USER = env("DHIS2_USER")
+    DHIS2_PASS = env("DHIS2_PASS")
+
+    cmd = Command()
+    auth = (DHIS2_USER, DHIS2_PASS)
+    #eventsnum = cmd.getEventsValues('sv91bCroFFx', auth)
+    va_in_dhis2 = cmd.getPushedVA('sv91bCroFFx', auth)
+    eventsnum = len(va_in_dhis2)
+    dhisdata = [int(i) for i in va_in_dhis2]
+
+    #cmd.syncDHISStatus()
+    vadata = VerbalAutopsy.objects.filter(causes__isnull=False, dhisva__isnull=True).exclude(id__in=dhisdata).count()
+    if vadata>0:
+        numPushed,numTotal,status = cmd.handle()
+    else:
+        cmd.syncDHISStatus()
+        status = 'Nothing to push'
     btnpush = 'OK'
+
     if status=='SUCCESS':
         txt = "successfully posted "+str(numPushed)+" out of "+str(numTotal)+" records"
     else:
-        txt = "push failed"
+        txt = "Nothing to Post!"
 
     context = {
         "txt":txt,
-        "btnpush":btnpush
+        "vanum": str(vadata),
+        "btnpush":btnpush,
+        'total':eventsnum
     }
     return render(request,"pages/dhis.html",context)
--- va_explorer/va_data_management/management/commands/run_dhis.py
+++ va_explorer/va_data_management/management/commands/run_dhis.py
@@ -8,6 +8,7 @@ import csv, environ
 #from . import dhis as dhis
 import openva_pipeline.dhis as dhis
 import pandas as pd
+import numpy as np
 
 # TODO: Temporary script to run COD assignment algorithms; this should
 # eventually become something that's handle with celery
@@ -45,124 +46,192 @@ class Command(BaseCommand):
 
         metadatacode = "InterVA5|5|Custom|1|2016 WHO Verbal Autopsy Form|v1_5_1"
 
+        #auth = (DHIS2_USER, DHIS2_PASS)
+        #va_in_dhis2 = self.getPushedVA('sv91bCroFFx', auth)
+        #dhisdata = [int(i) for i in va_in_dhis2]
+
         # Load all verbal autopsies that have been pushed to dhis2
         dhisdata = dhisStatus.objects.values_list("verbalautopsy_id", flat=True)
 
         #to subset few rows,add at the end [:10] for 10 rows etc..
         #exclude vas that have no dhis2 status; not pushed
-        vadata = VerbalAutopsy.objects.filter(causes__isnull=False).exclude(id__in=set(dhisdata)) #[:30]
+        vadata = VerbalAutopsy.objects.filter(causes__isnull=False).exclude(id__in=dhisdata) #[:30]
 
         #ceate a list of available VA IDs to help during filtering queries
-        list1 = list()
+        va_not_in_dhis = list()
         for va in vadata:
-            list1.append(va.id)
+            va_not_in_dhis.append(va.id)
+        va_not_in_dhis = [str(i) for i in va_not_in_dhis]
 
-        #load VAs with causes
-        cod = CauseOfDeath.objects.filter(verbalautopsy_id__in=list1).values()
-        cod = pd.DataFrame.from_records(cod)
-        cod = cod[{"verbalautopsy_id","cause"}]
+        if len(va_not_in_dhis)>0:
 
-        # Get into CSV format, also prefixing keys with - as expected by pyCrossVA (e.g. Id10424 becomes -Id10424)
-        va_data = [model_to_dict(va) for va in vadata]
-        vadf = pd.DataFrame.from_records(va_data)
+            #load VAs with causes
+            cod = CauseOfDeath.objects.filter(verbalautopsy_id__in=va_not_in_dhis).values()
+            cod = pd.DataFrame.from_records(cod)
+            cod = cod[{"verbalautopsy_id","cause"}]
 
-        codva = cod.join(vadf, how='outer')
+            # Get into CSV format, also prefixing keys with - as expected by pyCrossVA (e.g. Id10424 becomes -Id10424)
+            va_data = [model_to_dict(va) for va in vadata]
+            vadf = pd.DataFrame.from_records(va_data)
 
-        va_data = [dict([(f'-{k}', v) for k, v in d.items()]) for d in va_data]
-        va_data_csv = pd.DataFrame.from_records(va_data).to_csv()
+            codva = cod.join(vadf, how='outer')
 
-        # Transform to algorithm format using the pyCrossVA web service
-        transform_url = 'http://127.0.0.1:5001/transform?input=2016WHOv151&output=InterVA5'
-        transform_response = requests.post(transform_url, data=va_data_csv)
+            va_data = [dict([(f'-{k}', v) for k, v in d.items()]) for d in va_data]
+            va_data_csv = pd.DataFrame.from_records(va_data).to_csv()
 
-        # We need to convert the resulting CSV to JSON
-        transform_response_reader = csv.DictReader(StringIO(transform_response.text))
-        algorithm_input_rows = []
-        for row in transform_response_reader:
-            # Replace blank key with ID and append to list for later jsonification
-            algorithm_input_rows.append(OrderedDict([('ID', v) if k == '' else (k, v) for k, v in row.items()]))
+            # Transform to algorithm format using the pyCrossVA web service
+            transform_url = 'http://127.0.0.1:5001/transform?input=2016WHOv151&output=InterVA5'
+            transform_response = requests.post(transform_url, data=va_data_csv)
 
-        crossva = pd.DataFrame(algorithm_input_rows)
-        crossva = crossva.replace("0.0",".").replace("1.0","y")
-        list1 = [str(i) for i in list1]
-        crossva['ID']= list1
+            # We need to convert the resulting CSV to JSON
+            transform_response_reader = csv.DictReader(StringIO(transform_response.text))
+            algorithm_input_rows = []
+            for row in transform_response_reader:
+                # Replace blank key with ID and append to list for later jsonification
+                algorithm_input_rows.append(OrderedDict([('ID', v) if k == '' else (k, v) for k, v in row.items()]))
 
-        crossvacod = cod.join(crossva, how='outer')
-        crossvacod['Cause of Death']= crossvacod['cause']
-        crossvacod['Metadata'] = metadatacode
-        crossvacod = crossvacod.drop(['verbalautopsy_id','cause'], axis=1)
+            crossva = pd.DataFrame(algorithm_input_rows)
+            crossva = crossva.replace("0.0",".").replace("1.0","y")
 
-        eadata = self.generateEntityAttribute(crossvacod)
+            crossva['ID'] = va_not_in_dhis
 
-        #append a letter A to IDs; for some reason it was failing having numeric IDs
-        # producing KeyError trying to compare string/integer numbers
-        eadata['ID'] =["A"+str(i) for i in eadata['ID']]
-        if os.path.exists("OpenVAFiles"):
-            eadata.to_csv("OpenVAFiles/entityAttributeValue.csv", index=False)
+            crossvacod = cod.join(crossva, how='outer')
+            crossvacod['Cause of Death']= crossvacod['cause']
+            crossvacod['Metadata'] = metadatacode
+            crossvacod = crossvacod.drop(['verbalautopsy_id','cause'], axis=1)
+
+            eadata = self.generateEntityAttribute(crossvacod)
+
+            #append a letter A to IDs; for some reason it was failing having numeric IDs
+            # producing KeyError trying to compare string/integer numbers
+            eadata['ID'] =["A"+str(i) for i in eadata['ID']]
+            if os.path.exists("OpenVAFiles"):
+                eadata.to_csv("OpenVAFiles/entityAttributeValue.csv", index=False)
+            else:
+                os.makedirs("OpenVAFiles")
+                eadata.to_csv("OpenVAFiles/entityAttributeValue.csv", index=False)
+
+            codva['metadataCode'] = metadatacode
+            codva['odkMetaInstanceID'] = codva['id']
+            vasubset= codva[{"id","Id10019","Id10021","Id10023","ageInYears2","cause","metadataCode","odkMetaInstanceID"}]
+            vasubset = vasubset.rename(columns={"Id10019":"sex", "Id10021":"dob",
+                                                "Id10023":"dod","ageInYears2":"age",
+                                                "cause":"cod"})
+            vasubset = vasubset[['id', 'sex', 'dob', 'dod', 'age', 'cod', 'metadataCode','odkMetaInstanceID']]
+
+
+            storagev = vasubset.join(crossva, how='outer')
+            storagev= storagev.drop(['ID'], axis=1)
+            storagev.to_csv("OpenVAFiles/recordStorage.csv", index=False)
+
+            # Ensure all variables are formatted as required
+            vadata = pd.read_csv("OpenVAFiles/recordStorage.csv")
+            vadata["age"] = vadata["age"].astype(float)
+            vadata['dod'][vadata.dod.isnull()] = '1900-01-01'
+            vadata['dob'][vadata.dob.isnull()] = '1900-01-01'
+            vadata['id'] = ["A" + str(i) for i in vadata['id']]
+            for i in range(len(vadata["sex"])):
+                vadata["dob"][i] = dateutil.parser.parse(vadata["dob"][i]).date().strftime("%Y-%m-%d")
+                vadata["dod"][i] = dateutil.parser.parse(vadata["dod"][i]).date().strftime("%Y-%m-%d")
+                vadata["cod"][i] = vadata["cod"][i].strip()
+                vadata["metadataCode"][i] =  metadatacode
+            vadata.to_csv("OpenVAFiles/recordStorage.csv", index=False)
+
+            # dhis settings
+            ntDHIS = collections.namedtuple("ntDHIS",
+                                            ["dhisURL",
+                                             "dhisUser",
+                                             "dhisPassword",
+                                             "dhisOrgUnit"]
+                                            )
+            settingsDHIS = ntDHIS(DHIS2_URL, DHIS2_USER, DHIS2_PASS, DHIS2_ORGUNIT)
+
+            CODCodes =  cod_codes_dhis.objects.filter(codsource="WHO").values()
+            queryCODCodes = pd.DataFrame.from_records(CODCodes)
+            queryCODCodes = queryCODCodes[{"codname", "codcode"}]
+
+            dhisCODCodes = dict(zip(queryCODCodes.codname, queryCODCodes.codcode))
+            argsDHIS = [settingsDHIS, dhisCODCodes]
+
+            # execute pipeline
+            pipelineDHIS = dhis.DHIS(argsDHIS,"")
+            apiDHIS = pipelineDHIS.connect()
+            postLog = pipelineDHIS.postVA(apiDHIS)
+            self.clearFolder("DHIS/blobs/")
+            if postLog['response']['status']=='SUCCESS' and postLog['response']['imported']==len(va_not_in_dhis):
+                numPushed = postLog['response']['imported']
+                numTotal = postLog['response']['total']
+                status = postLog['response']['status']
+            self.stdout.write(f' Uploaded {numPushed} out of {numTotal} verbal autopsies ')
+
         else:
-            os.makedirs("OpenVAFiles")
-            eadata.to_csv("OpenVAFiles/entityAttributeValue.csv", index=False)
+            numPushed=0
+            numTotal=0
+            status="Nothing to Post"
 
-        codva['metadataCode'] = metadatacode
-        codva['odkMetaInstanceID'] = codva['id']
-        vasubset= codva[{"id","Id10019","Id10021","Id10023","ageInYears2","cause","metadataCode","odkMetaInstanceID"}]
-        vasubset = vasubset.rename(columns={"Id10019":"sex", "Id10021":"dob",
-                                            "Id10023":"dod","ageInYears2":"age",
-                                            "cause":"cod"})
-        vasubset = vasubset[['id', 'sex', 'dob', 'dod', 'age', 'cod', 'metadataCode','odkMetaInstanceID']]
+        self.syncDHISStatus()
 
-
-        storagev = vasubset.join(crossva, how='outer')
-        storagev= storagev.drop(['ID'], axis=1)
-        storagev.to_csv("OpenVAFiles/recordStorage.csv", index=False)
-
-        # Ensure all variables are formatted as required
-        vadata = pd.read_csv("OpenVAFiles/recordStorage.csv")
-        vadata["age"] = vadata["age"].astype(float)
-        vadata['dod'][vadata.dod.isnull()] = '1900-01-01'
-        vadata['dob'][vadata.dob.isnull()] = '1900-01-01'
-        vadata['id'] = ["A" + str(i) for i in vadata['id']]
-        for i in range(len(vadata["sex"])):
-            vadata["dob"][i] = dateutil.parser.parse(vadata["dob"][i]).date().strftime("%Y-%m-%d")
-            vadata["dod"][i] = dateutil.parser.parse(vadata["dod"][i]).date().strftime("%Y-%m-%d")
-            vadata["cod"][i] = vadata["cod"][i].strip()
-            vadata["metadataCode"][i] =  metadatacode
-        vadata.to_csv("OpenVAFiles/recordStorage.csv", index=False)
-
-        # dhis settings
-        ntDHIS = collections.namedtuple("ntDHIS",
-                                        ["dhisURL",
-                                         "dhisUser",
-                                         "dhisPassword",
-                                         "dhisOrgUnit"]
-                                        )
-        settingsDHIS = ntDHIS(DHIS2_URL, DHIS2_USER, DHIS2_PASS, DHIS2_ORGUNIT)
-
-        CODCodes =  cod_codes_dhis.objects.filter(codsource="WHO").values()
-        queryCODCodes = pd.DataFrame.from_records(CODCodes)
-        queryCODCodes = queryCODCodes[{"codname", "codcode"}]
-
-        dhisCODCodes = dict(zip(queryCODCodes.codname, queryCODCodes.codcode))
-        argsDHIS = [settingsDHIS, dhisCODCodes]
-
-        # execute pipeline
-        pipelineDHIS = dhis.DHIS(argsDHIS,"")
-        apiDHIS = pipelineDHIS.connect()
-        postLog = pipelineDHIS.postVA(apiDHIS)
-        self.clearFolder("DHIS/blobs/")
-        if postLog['response']['status']=='SUCCESS' and postLog['response']['imported']==len(list1):
-            self.InsertDHISStatus(list1)
-            numPushed = postLog['response']['imported']
-            numTotal = postLog['response']['total']
-            status = postLog['response']['status']
-        self.stdout.write(f' Uploaded {numPushed} out of {numTotal} verbal autopsies ')
-        ## ADD Function to post to table dhis tracker
         return numPushed,numTotal,status
 
-    def InsertDHISStatus(self,list1):
-        for item in list1:
-            ds = dhisStatus.objects.create(vaid=item,verbalautopsy_id=item)
+    def syncDHISStatus(self):
+        env = environ.Env()
+
+        # DHIS2 VARIABLES
+        DHIS2_USER = env("DHIS2_USER")
+        DHIS2_PASS = env("DHIS2_PASS")
+
+        auth = (DHIS2_USER, DHIS2_PASS)
+        va_in_dhis2 = self.getPushedVA('sv91bCroFFx', auth)
+        va_in_dhis2 = [str(i) for i in va_in_dhis2]
+
+        #print(va_in_dhis2)
+        dhisdata = dhisStatus.objects.filter(verbalautopsy_id__isnull=False).values_list('vaid',flat=True)
+        dhisdata = list(dhisdata)
+        dhisdata = [str(i) for i in dhisdata]
+
+        #remove orphaned items deleted in dhis2
+        for x in dhisdata:
+            if x not in va_in_dhis2:
+                ds = dhisStatus.objects.get(vaid=x)
+                ds.delete()
+
+        dhisdata = dhisStatus.objects.filter(verbalautopsy_id__isnull=False).values_list('vaid', flat=True)
+        dhisdata = list(dhisdata)
+        dhisdata = [str(i) for i in dhisdata]
+
+        toinsert = np.setdiff1d(va_in_dhis2, dhisdata)
+        toinsert = list(toinsert)
+
+        for item in toinsert:
+            ds = dhisStatus.objects.create(vaid=item,verbalautopsy_id=int(item))
             ds.save()
 
+    def getEventsValues(self,prg, auth):
+        env = environ.Env()
+        # DHIS2 VARIABLES
+        DHIS2_URL = env("DHIS2_URL")
+        DHIS2_ORGUNIT = env("DHIS2_ORGUNIT")
+        url = DHIS2_URL+'/api/events?pageSize=0&program=' + prg + '&orgUnit=' + DHIS2_ORGUNIT + '&totalPages=true'
+        r = requests.get(url, auth=auth)
+        jn = r.json()
+        return jn['pager']['total']
 
+    def getPushedVA(self,prg, auth):
+        env = environ.Env()
+        # DHIS2 VARIABLES
+        DHIS2_URL = env("DHIS2_URL")
+        DHIS2_ORGUNIT = env("DHIS2_ORGUNIT")
+
+        eventsnum = self.getEventsValues(prg, auth)
+        url = DHIS2_URL+'/api/events?pageSize=' + format(eventsnum,'0') + '&program=' + prg + '&orgUnit=' + DHIS2_ORGUNIT + '&totalPages=true'
+        r = requests.get(url, auth=auth)
+        jn = r.json()
+        list1 = list()
+        for i in range(eventsnum):
+            cols = len(jn['events'][i]['dataValues'])
+            for j in range(cols):
+                if jn['events'][i]['dataValues'][j]['dataElement'] == 'LwXZ2dZmJb0':
+                    txt = jn['events'][i]['dataValues'][j]['value']
+                    list1.append(txt)
+        return list1
 
