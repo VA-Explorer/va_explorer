a44eff8b9e4a38bedc840b598f89962dc333f43c
Pete K <krautscheid@mitre.org>
2021-03-03 17:26:16 -0500

    Add tests for home page with metrics

---
+++ va_explorer/home/tests/test_views.py
@@ -0,0 +1,38 @@
+import pytest
+from django.test import Client
+from va_explorer.users.models import User
+from va_explorer.va_data_management.models import VerbalAutopsy
+from va_explorer.tests.factories import VerbalAutopsyFactory, LocationFactory
+
+pytestmark = pytest.mark.django_db
+
+# Get the index and make sure 
+def test_index(user: User):
+    client = Client()
+    client.force_login(user=user)
+    va = VerbalAutopsyFactory.create()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.context['vas_collected_24_hours'] == 1
+    assert response.context['vas_collected_1_week'] == 1
+    assert response.context['vas_collected_1_month'] == 1
+    assert response.context['vas_collected_overall'] == 1
+    assert response.context['vas_coded_24_hours'] == 0
+    assert response.context['vas_coded_1_week'] == 0
+    assert response.context['vas_coded_1_month'] == 0
+    assert response.context['vas_coded_overall'] == 0
+    assert response.context['vas_uncoded_24_hours'] == 1
+    assert response.context['vas_uncoded_1_week'] == 1
+    assert response.context['vas_uncoded_1_month'] == 1
+    assert response.context['vas_uncoded_overall'] == 1
+    assert len(response.context['issue_list']) == 1
+    assert response.context['issue_list'][0]['name'] == va.Id10007
+    assert response.context['additional_issues'] == 0
+
+# Get the about page and make sure it returns successfully
+def test_about(user: User):
+    client = Client()
+    client.force_login(user=user)
+    va = VerbalAutopsyFactory.create()
+    response = client.get(f"/about/")
+    assert response.status_code == 200
