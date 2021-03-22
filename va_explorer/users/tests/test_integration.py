579d772c23dcef94ce51fc6f625b355667086034
Diana Eastman <deastman@mitre.org>
2021-03-22 10:46:33 -0400

    Update tests to reflect newly added va_username field on user create and update forms.

--- va_explorer/users/tests/test_integration.py
+++ va_explorer/users/tests/test_integration.py
@@ -4,12 +4,18 @@ from django.core import mail
 from django.test import Client, RequestFactory
 from django.urls import reverse
 
-from va_explorer.users.forms import ExtendedUserCreationForm
+from va_explorer.users.forms import ExtendedUserCreationForm, UserUpdateForm
 from va_explorer.tests.factories import (
     GroupFactory,
     LocationFactory,
-    NewUserFactory, UserFactory,
+    NewUserFactory,
+    UserFactory,
+    FieldWorkerFactory,
+    FacilityFactory,
+    FieldWorkerGroupFactory,
+    VaUsernameFactory,
 )
+from va_explorer.va_data_management.models import VaUsername
 
 pytestmark = pytest.mark.django_db
 
@@ -49,6 +55,27 @@ def create_and_return_a_new_user(rf, proto_user):
     return user
 
 
+def update_and_return_a_field_worker(rf, field_worker, group, va_username, facility):
+    form = UserUpdateForm(
+        {
+            "name": field_worker.name,
+            "email": field_worker.email,
+            "group": group,
+            "geographic_access": "location-specific",
+            "location_restrictions": [facility],
+            "va_username": va_username
+        }
+    )
+
+    request = rf.get("/fake-url/")
+    form.request = request
+    form.instance = field_worker
+
+    field_worker_updated = form.save()
+
+    return field_worker_updated
+
+
 def test_user_creation(rf: RequestFactory):
     # A user with proto_user params does not exist yet.
     proto_user = NewUserFactory.build()
@@ -75,6 +102,49 @@ def test_user_creation(rf: RequestFactory):
     # See: https://docs.djangoproject.com/en/3.0/ref/contrib/auth/#django.contrib.auth.models.User.check_password
     password = retrieve_password_from_email_body(email.body)
     assert user.check_password(password) is True
+
+
+def test_field_worker_update_va_username(rf: RequestFactory):
+    field_worker_group = FieldWorkerGroupFactory.create()
+    field_worker = FieldWorkerFactory.create(groups=[field_worker_group])
+
+    facility = FacilityFactory.create()
+    field_worker.location_restrictions.add(*[facility])
+
+    field_worker_username = VaUsernameFactory.create(user=field_worker)
+
+    field_worker.save()
+    field_worker_username.save()
+
+    new_username = "updated_username"
+
+    field_worker_updated = update_and_return_a_field_worker(
+        rf, field_worker, field_worker_group, new_username, facility
+    )
+
+    assert len(VaUsername.objects.filter(user=field_worker_updated)) == 1
+    assert VaUsername.objects.get(user=field_worker_updated).va_username == new_username
+
+
+def test_field_worker_update_delete_va_username(rf: RequestFactory):
+    field_worker_group = FieldWorkerGroupFactory.create()
+    field_worker = FieldWorkerFactory.create(groups=[field_worker_group])
+
+    facility = FacilityFactory.create()
+    field_worker.location_restrictions.add(*[facility])
+
+    field_worker_username = VaUsernameFactory.create(user=field_worker)
+
+    field_worker.save()
+    field_worker_username.save()
+
+    new_username = ""
+
+    field_worker_updated = update_and_return_a_field_worker(
+        rf, field_worker, field_worker_group, new_username, facility
+    )
+
+    assert len(VaUsername.objects.filter(user=field_worker_updated)) == 0
 
 
 def test_user_set_password_after_create(rf: RequestFactory):
