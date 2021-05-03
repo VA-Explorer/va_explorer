52d0f9d8ad1f422bebb145115465a36a156fa261
Tim Shaffer <timshaffer@me.com>
2021-05-03 11:03:05 -0400

    Fix for users with blank usernames (#84)
    * Fix for users with blank usernames.
    
    * Fix when no va_username is in form.
    
    * Fix issue with missing user.save().
    
    Co-authored-by: Tim Shaffer <tshaffer@mitre.org>

--- va_explorer/users/forms.py
+++ va_explorer/users/forms.py
@@ -201,21 +201,15 @@ class ExtendedUserCreationForm(UserCommonFields, UserCreationForm):
             location_restrictions = get_location_restrictions(self.cleaned_data)
             group = self.cleaned_data["group"]
 
-            va_username = None
-
-            if self.cleaned_data["va_username"]:
-                va_username = VaUsername(va_username=self.cleaned_data["va_username"], user=user)
-
             if commit:
+                # Need to save again after setting password.
                 user.save()
 
                 # You cannot associate the user with a m2m field until it’s been saved
                 # https://docs.djangoproject.com/en/3.1/topics/db/examples/many_to_many/
                 user.location_restrictions.add(*location_restrictions)
                 user.groups.add(*[group])
-
-                if va_username:
-                    va_username.save()
+                user.set_va_username(self.cleaned_data.get("va_username"))
 
                 # If selected group cannot view PII, modify the user's permissions according to the checkbox.
                 if not self.cleaned_data['group'].permissions.filter(codename='view_pii').first():
@@ -291,7 +285,7 @@ class UserUpdateForm(UserCommonFields, forms.ModelForm):
             """
             user.location_restrictions.set(location_restrictions)
             user.groups.set([group])
-            user.set_va_username(self.cleaned_data["va_username"])
+            user.set_va_username(self.cleaned_data.get("va_username"))
 
             # If selected group cannot view PII, modify the user's permissions according to the checkbox.
             if not self.cleaned_data['group'].permissions.filter(codename='view_pii').first():
