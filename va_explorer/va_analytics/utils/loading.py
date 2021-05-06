40408ab45e5be6b7304623bd7e3735e2540e0a5d
Tim Shaffer <tshaffer@mitre.org>
2021-05-06 16:11:49 -0400

    Optimize load_va_data.

--- va_explorer/va_analytics/utils/loading.py
+++ va_explorer/va_analytics/utils/loading.py
@@ -6,7 +6,9 @@ Created on Tue Apr 13 10:08:05 2021
 @author: babraham
 """
 
-
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
+from django.db.models import F
 import pandas as pd
 import numpy as np
 import json
@@ -69,94 +71,109 @@ def load_geojson_data(json_file):
 
 # ============ VA Data =================
 def load_va_data(user, geographic_levels=None, date_cutoff="1901-01-01"):
-    np.random.seed(23)
-    return_dict = {"data": {"valid": pd.DataFrame(), "invalid": pd.DataFrame()}}
-    
     # the dashboard requires date of death, exclude if the date is unknown
-    all_vas = user.verbal_autopsies(date_cutoff=date_cutoff).exclude(Id10023="dk").exclude(location__isnull=True).prefetch_related("location").prefetch_related("causes")
+    # Using .values at the end lets us do select_related("causes") which drastically speeds up the query.
+    all_vas = user \
+        .verbal_autopsies(date_cutoff=date_cutoff) \
+        .only(
+            "id",
+            "Id10019",
+            "Id10058",
+            "Id10023",
+            "ageInYears",
+            "age_group",
+            "isNeonatal1",
+            "isChild1",
+            "isAdult1",
+            "location",
+        ) \
+        .exclude(Id10023="dk") \
+        .exclude(location__isnull=True) \
+        .select_related("location") \
+        .select_related("causes") \
+        .values(
+            "id",
+            "Id10019",
+            "Id10058",
+            "age_group",
+            "isNeonatal1",
+            "isChild1",
+            "isAdult1",
+            'location__id',
+            'location__name',
+            'ageInYears',
+            age=F("ageInYears"),
+            date=F("Id10023"),
+            cause=F("causes__cause"),
+        )
     
-    if len(all_vas) > 0:
-        # Grab exactly the fields we need, including location and cause data
-        va_data = [
-            {
-                "id": va.id,
-                "Id10019": va.Id10019,
-                "Id10058": va.Id10058,
-                "date" : va.Id10023, # date of death assignment
-                "ageInYears": va.ageInYears,
-                "age_group": va.age_group,
-                "isNeonatal1": va.isNeonatal1,
-                "isChild1": va.isChild1,
-                "isAdult1": va.isAdult1,
-                "location": va.location.name,
-                "cause": va.causes.first()
-            }
-            for va in all_vas
-        ]
+    if not all_vas:
+        return {"data": {"valid": pd.DataFrame(), "invalid": pd.DataFrame()}}
 
-        # Build a location ancestors lookup and add location information at all levels to all vas
-        # TODO: This is not efficient (though it's better than 2 DB queries per VA)
-        # TODO: This assumes that all VAs will occur in a facility, ok? 
-        # TODO: if there is no location data, we could use the location associated with the interviewer
-        locations, location_types = dict(), dict()
-        location_ancestors = {
-            location.id: location.get_ancestors()
-            for location in Location.objects.filter(location_type="facility")
-        }
-        for i, va in enumerate(all_vas):
-            #            location_types[va.location.depth] = va.location.location_type
-            #            locations.add(va.location.name)
-            for ancestor in location_ancestors[va.location.id]:
-                va_data[i][ancestor.location_type] = ancestor.name
-                location_types[ancestor.depth] = ancestor.location_type
-                locations[ancestor.name] = ancestor.location_type
-        va_df = pd.DataFrame.from_records(va_data)
+    # Build a dictionary of location ancestors for each facility
+    # TODO: This is not efficient (though it"s better than 2 DB queries per VA)
+    # TODO: This assumes that all VAs will occur in a facility, ok? 
+    # TODO: if there is no location data, we could use the location associated with the interviewer
+    location_types = set()
+    locations = {}
+    location_ancestors = {
+        location.id: location.get_ancestors()
+        for location in Location.objects.filter(location_type="facility")
+    }
 
-        # Set the age field so we can calculate mean age of death
-        va_df["age"] = pd.to_numeric(va_df["ageInYears"], errors="coerce")
+    for va in all_vas:
+        # Find parents (likely district and province).
+        for ancestor in location_ancestors[va['location__id']]:
+            va[ancestor.location_type] = ancestor.name
+            location_types.add(ancestor.location_type)
+            locations[ancestor.name] = ancestor.location_type
 
-        # Assign to age group
-        va_df["age_group"] = va_df.apply(assign_age_group, axis=1)
+        # Clean up location fields.
+        va["location"] = va["location__name"]
+        del va["location__name"]
+        del va["location__id"]
 
-        # split data into valid data (records w COD) and invalid records (recoreds w/out COD)
-        valid_va_df = va_df[~pd.isnull(va_df["cause"])].reset_index()
-        invalid_va_df = va_df[pd.isnull(va_df["cause"])].reset_index()
-        # convert location_types to an ordered list
-        location_types = [
-            l for _, l in sorted(location_types.items(), key=lambda x: x[0])
-        ]
-        return_dict = {
-            "data": {"valid": valid_va_df, "invalid": invalid_va_df},
-            "location_types": location_types,
-            "max_depth": len(location_types) - 1,
-            "locations": locations,
-        }
+        # Check age group
+        va["age_group"] = get_age_group(va)
 
-    return return_dict
+    # Convert list to dataframe.
+    va_df = pd.DataFrame.from_records(all_vas)
+    va_df["age"] = pd.to_numeric(va_df["age"], errors="coerce")
+    
+    return {
+        "data": {
+            "valid": va_df[~pd.isnull(va_df["cause"])].reset_index(),
+            "invalid": va_df[pd.isnull(va_df["cause"])].reset_index(),
+        },
+        "location_types": sorted(location_types),
+        "max_depth": len(location_types) - 1,
+        "locations": locations,
+    }
 
 
-def assign_age_group(va_row):
+def get_age_group(va):
     # If age group is unassigned, determine age group by age group fields first, then age number, otherwise mark NA
     # TODO determine if this is a valid check for empty or unknown values
-    if va_row["age_group"] not in ["adult", "neonate", "child"]: 
-        if va_row["isNeonatal1"] == 1:
+
+    if va["age_group"] in ["adult", "neonate", "child"]: 
+        return va["age_group"]
+
+    if va["isNeonatal1"] == 1:
+        return "neonate"
+
+    if va["isChild1"] == 1:
+        return "child"
+    
+    if va["isAdult1"] == 1:
+        return "adult"
+    
+    # try determine group by the age in years
+    try:
+        age = int(float(va["age"]))
+        if age <= 1:
             return "neonate"
-        elif va_row["isChild1"] == 1:
+        if age <= 16:
             return "child"
-        elif va_row["isAdult1"] == 1:
-            return "adult"
-        else:
-            # try determine group by the age in years
-            try:
-                age = int(va_row["age"])
-                if age <= 1:
-                    return "neonate"
-                if age <= 16:
-                    return "child"
-                return "adult"
-            except:
-                return "Unknown"
-    return va_row["age_group"]
-
-
-
+        return "adult"
+    except:
+        return "Unknown"
