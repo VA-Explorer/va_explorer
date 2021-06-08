c5a91786bee8113f2568edbbe2d426a3cb7bf40f
Abraham, Brendan <babraham@mitre.org>
2021-06-08 12:29:57 -0400

    improved bulk upload script to handle user roles and location restrictions. Still need to handle permissions and clean up code.

--- va_explorer/users/utils.py
+++ va_explorer/users/utils.py
@@ -8,11 +8,85 @@ Created on Mon Apr 12 14:33:57 2021
 
 from django.contrib.auth.models import Group
 from django.db.models import F
+from django.contrib.auth.models import Permission
+from django.utils.crypto import get_random_string
+
 
 from va_explorer.users.models import User
 from va_explorer.va_data_management.models import Location
 
 import pandas as pd
+import re
+
+
+
+def get_app_permissions(app_names=None):
+    perms = Permission.objects.values_list('content_type__app_label', 'codename')
+    perm_df = pd.DataFrame.from_records(perms, columns=['app', 'codename'])
+    if app_names:
+        perm_df = perm_df.query("app==@app_names")
+        if perm_df.shape == 0:
+            print(f"WARNING: couldnt find permissions for app(s) {app_names}")
+    return perm_df
+
+
+# bulk create a list of users from a csv file. Can specify name, email, group (user role), location restrictions, and permissions
+def create_users_from_csv(user_list_file):
+    user_df = pd.read_csv(user_list_file)
+    # initialize counters and dictionaries
+    new_user_ct = 0
+    # dictionaries for group and location matching
+    group_dict = {g.name.lower(): g for g in Group.objects.all()}
+    location_dict = {l.name.lower(): l for l in Location.objects.all()}
+    
+    for i, user_data in user_df.iterrows():
+        # check if email already exists
+        name, email = user_data['name'], user_data['email']
+        if not re.match('[^@]+@[^@]+\.[^@]+', email):
+            print(f'Invalid email address found: {email}')
+            continue
+        if len(name.strip()) == 0:
+            name = email
+        if User.objects.filter(email=email).exists():
+            print(f'User account already exists for email {email}')
+        else:
+            user, created = User.objects.get_or_create(email=email, defaults={'name': name, 'is_active': True})
+            if created:
+                
+                password = get_random_string(length=8)
+                user.set_password(password)
+                # Need to save after setting password.
+                user.save()
+                
+                # determine and set user group if provided
+                group = user_data.get("group", None)
+                if not pd.isnull(group):
+                    group = group.lower()
+                    group += "s" if not group.endswith('s') else group
+                    known_group = group_dict.get(group, None)
+                    if known_group:
+                        user.groups.add(*[known_group])
+                    else:
+                        print(f"WARNING: couldnt match {user_data['group']} to known groups")
+                       
+                # determine and set location restirctions if provided. For now, assumes one location provided
+                restriction = user_data.get("location_restrictions", None)
+                if not pd.isnull(restriction):
+                    # try to match location to known db locations
+                    known_location = location_dict.get(restriction.lower(), None)
+                    if  known_location:
+                        user.location_restrictions.add(*[known_location])
+                    else:
+                        print(f"WARNING: couldnt match {user_data['location_restrictions']} to known locations")
+                        
+                # TODO: parse user permission
+                
+                # save again to update user fields
+                user.save()
+                print(f'Created account for user {name} with email {email} and temporary password {password}')
+                new_user_ct +=1
+                
+    print(f"Created {new_user_ct} new users")
 
 # get table with basic info for all users in system. No PII included in result
 def get_anonymized_user_info():      
@@ -23,7 +97,7 @@ def get_anonymized_user_info():
             'uuid',
             'is_active',
             'is_staff',
-            account_type = F('groups__name'),
+            role = F('groups__name'),
             locations = F('location_restrictions__name')           
     )
     
