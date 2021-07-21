e7604cd8184143a1e196c72efeff540d4c5ec34a
babraham33 <55208284+babraham33@users.noreply.github.com>
2021-07-21 11:30:05 -0400

    fixed pagination template issue (#117)

--- va_explorer/templatetags/va_explorer_tags.py
+++ va_explorer/templatetags/va_explorer_tags.py
@@ -28,3 +28,25 @@ def pii_filter(context, field, value):
     if field in PII_FIELDS and not context['user'].can_view_pii:
         return REDACTED_STRING
     return value
+
+@register.simple_tag(takes_context=True)
+def param_replace(context, **kwargs):
+    """
+    Return encoded URL parameters that are the same as the current
+    request's parameters, only with the specified GET parameters added or changed.
+    It also removes any empty parameters to keep things neat,
+    so you can remove a parm by setting it to ``""``.
+    For example, if you're on the page ``/things/?with_frosting=true&page=5``,
+    then
+    <a href="/things/?{% param_replace page=3 %}">Page 3</a>
+    would expand to
+    <a href="/things/?with_frosting=true&page=3">Page 3</a>
+    Based on
+    https://stackoverflow.com/questions/22734695/next-and-before-links-for-a-django-paginated-query/22735278#22735278
+    """
+    d = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        d[k] = v
+    for k in [k for k, v in d.items() if not v]:
+        del d[k]
+    return d.urlencode()
