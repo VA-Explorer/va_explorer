88456c554f442b2441526fb86c70d421dfa7aeea
Abraham, Brendan <babraham@mitre.org>
2021-07-22 11:46:29 -0400

    changed group and user ownership to django user/group to prevent build issue

--- va_explorer/va_logs/management/commands/initialize_log_directory.py
+++ va_explorer/va_logs/management/commands/initialize_log_directory.py
@@ -9,21 +9,28 @@ class Command(BaseCommand):
     help = "Initialize log directories for event loggig and debugging"
 
     def handle(self, *args, **options):
-    	if not os.path.isdir(LOG_DIR):
-    		try:
-    			os.mkdir(LOG_DIR)
-    			print(f"Made log directory at {LOG_DIR}")
-    		except:
-    			raise FileNotFoundError(f"Couldnt create log directory {LOG_DIR}")
-    	handlers = LOGGING["handlers"]
-    	for handler_name, cfg in handlers.items():
-    		logfile = cfg.get("filename", None)
-    		if logfile:
-    			if not os.path.isfile(logfile):
-    				try:
-    					with open(logfile, "w") as out:
-    						pass
-    					print(f"Made new logfile {logfile}")
-    				except:
-    					raise FileNotFoundError(f"Couldnt create log file {logfile}")
+        if not os.path.isdir(LOG_DIR):
+            try:
+                os.mkdir(LOG_DIR)
+                try:
+                    os.chown(LOG_DIR, gid=101, uid=101)
+                except:
+                    print(f"failed to change ownership of {LOG_DIR}")
+                print(f"Made log directory at {LOG_DIR}")
+                
+            except:
+                raise FileNotFoundError(f"Couldnt create log directory {LOG_DIR}")
+            
+        handlers = LOGGING["handlers"]
+        for handler_name, cfg in handlers.items():
+            logfile = cfg.get("filename", None)
+            if logfile:
+                if not os.path.isfile(logfile):
+                    try:
+                        with open(logfile, "w") as out:
+                            pass
+                        print(f"Made new logfile {logfile}")
+                        
+                    except:
+                        raise FileNotFoundError(f"Couldnt create log file {logfile}")
 
