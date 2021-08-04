b9bd92bc143b62f6dd48c4f148e1d26abd311e91
Adam Barnes <ajbarnes@mitre.org>
2021-08-04 19:13:09 -0400

    Add cause of death as a filter. Add deceased as filter and only show interviewer filter to non-field worker roles

--- va_explorer/va_data_management/filters.py
+++ va_explorer/va_data_management/filters.py
@@ -3,7 +3,7 @@ from django import forms
 from django_filters import FilterSet, DateFilter, CharFilter, DateRangeFilter, BooleanFilter
 
 from .models import VerbalAutopsy
-
+from django.db.models import Q
 
 TRUE_FALSE_CHOICES = (
     (False, 'No'),
@@ -16,17 +16,26 @@ class DateInput(forms.DateInput):
 
 
 class VAFilter(FilterSet):
-	interviewer = CharFilter(field_name="Id10010", lookup_expr="icontains", label="Interviewer")
-	start_date = DateFilter(field_name="Id10023", lookup_expr="gte", label="Earliest Date", widget=DateInput(attrs={'class': 'datepicker'}))
-	end_date = DateFilter(field_name="Id10023", lookup_expr="lte", label="Latest Date", widget=DateInput(attrs={'class': 'datepicker'}))
-	location = CharFilter(field_name="location__name", lookup_expr="icontains", label="Facility")
-	only_errors = BooleanFilter(method="filter_errors", label="Only Errors", widget=forms.Select(choices=TRUE_FALSE_CHOICES, attrs={'class': 'custom-select'}))
+    interviewer = CharFilter(field_name="Id10010", lookup_expr="icontains", label="Interviewer")
+    deceased = CharFilter(method="filter_deceased", label="Deceased")
+    start_date = DateFilter(field_name="Id10023", lookup_expr="gte", label="Earliest Date", widget=DateInput(attrs={'class': 'datepicker'}))
+    end_date = DateFilter(field_name="Id10023", lookup_expr="lte", label="Latest Date", widget=DateInput(attrs={'class': 'datepicker'}))
+    location = CharFilter(field_name="location__name", lookup_expr="icontains", label="Facility")
+    cause = CharFilter(field_name="causes__cause", lookup_expr="icontains", label="Cause")
+    only_errors = BooleanFilter(method="filter_errors", label="Only Errors", widget=forms.Select(choices=TRUE_FALSE_CHOICES, attrs={'class': 'custom-select'}))
 
-	class Meta:
-		model = VerbalAutopsy
-		fields = []
+    class Meta:
+        model = VerbalAutopsy
+        fields = []
 
-	def filter_errors(self, queryset, name, value):
-		if value:
-			return queryset.filter(coding_issues__severity='error')
-		return queryset
+    def filter_deceased(self, queryset, name, value):
+        if value:
+            return VerbalAutopsy.objects.filter(
+                Q(Id10017__icontains=value) | Q(Id10018__icontains=value)
+            )
+        return queryset
+
+    def filter_errors(self, queryset, name, value):
+        if value:
+            return queryset.filter(coding_issues__severity='error')
+        return queryset
