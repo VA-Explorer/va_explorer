1f3149ae13ffe524eb37964ac9192c7ec3084dee
Adam Barnes <ajbarnes@mitre.org>
2021-08-30 17:09:30 -0400

    Fix broken bulk_create_users test by adding new supervise permission to fixture. Add missing dep to requirements/base.txt

--- va_explorer/users/tests/user_test_utils.py
+++ va_explorer/users/tests/user_test_utils.py
@@ -1,61 +1,80 @@
-from io import StringIO
-from pathlib import Path
-
-import pandas as pd
-import pytest
-import datetime
-from django.core.management import call_command
-
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from va_explorer.va_data_management.utils.loading import load_records_from_dataframe
+
+from va_explorer.tests.factories import (
+    LocationFactory,
+    VerbalAutopsyFactory,
+)
 from va_explorer.users.management.commands.initialize_groups import GROUPS_PERMISSIONS
-from va_explorer.tests.factories import UserFactory, LocationFactory, VerbalAutopsyFactory
+
 
 def setup_test_db(with_vas=True):
-	province = LocationFactory.create(name="Province1")
-	districtX = province.add_child(name='DistrictX', location_type='district')
-	facility1 = districtX.add_child(name='Facility1', location_type='facility')
-	districtY = province.add_child(name='DistrictY', location_type='district')
-	facility2 = districtY.add_child(name='Facility2', location_type='facility')
-	facility3 = districtY.add_child(name='Facility3', location_type='facility')
+    province = LocationFactory.create(name="Province1")
+    districtX = province.add_child(name="DistrictX", location_type="district")
+    facility1 = districtX.add_child(name="Facility1", location_type="facility")
+    districtY = province.add_child(name="DistrictY", location_type="district")
+    facility2 = districtY.add_child(name="Facility2", location_type="facility")
+    facility3 = districtY.add_child(name="Facility3", location_type="facility")
 
-	if with_vas:
-		# Each facility with one VA
-		va1 = VerbalAutopsyFactory.create(location=facility1)
-		va2 = VerbalAutopsyFactory.create(location=facility2)
-		va3 = VerbalAutopsyFactory.create(location=facility3)
+    if with_vas:
+        # Each facility with one VA
+        va1 = VerbalAutopsyFactory.create(location=facility1)
+        va2 = VerbalAutopsyFactory.create(location=facility2)
+        va3 = VerbalAutopsyFactory.create(location=facility3)
 
-	# create user groups defined in initialize_groups.py
-	for group_name, group_permissions in GROUPS_PERMISSIONS.items():
-		group, created = Group.objects.get_or_create(name=group_name)
-		if group.permissions.exists():
-			group.permissions.clear()
-		for model_class, model_permissions in group_permissions.items():
-			for codename in model_permissions:
-				# Get the content type for the given model class.
-				content_type = ContentType.objects.get_for_model(model_class)
+    # create user groups defined in initialize_groups.py
+    for group_name, group_permissions in GROUPS_PERMISSIONS.items():
+        group, created = Group.objects.get_or_create(name=group_name)
+        if group.permissions.exists():
+            group.permissions.clear()
+        for model_class, model_permissions in group_permissions.items():
+            for codename in model_permissions:
+                # Get the content type for the given model class.
+                content_type = ContentType.objects.get_for_model(model_class)
 
-				# Lookup permission based on content type and codename.
-				try:
-					permission = Permission.objects.get(content_type=content_type, codename=codename)
-					group.permissions.add(permission)
-				except:
-					pass
+                # Lookup permission based on content type and codename.
+                try:
+                    permission = Permission.objects.get(
+                        content_type=content_type, codename=codename
+                    )
+                    group.permissions.add(permission)
+                except:
+                    pass
 
-# create three dummy users for testing, each with different roles, location restirctions, and privacy privileges
+
+# create three dummy users for testing, each with different roles, location restrictions, and privacy privileges
 def get_fake_user_data():
-	users = {}
-	# user 1: restricted to location X, data viewer, cant view PII but can download data
-	users["u1"] = {'name': 'user1', 'email': 'user1@example.com', 'location_restrictions': 'DistrictX',
-	'group': 'Data Viewer', 'view_pii': False, 'download_data': True}
+    users = {}
+    # user 1: restricted to location X, data viewer, cant view PII but can download data
+    users["u1"] = {
+        "name": "user1",
+        "email": "user1@example.com",
+        "location_restrictions": "DistrictX",
+        "group": "Data Viewer",
+        "view_pii": False,
+        "download_data": True,
+        "supervise_users": False,
+    }
 
-	# user 2: data manager restricted to districtY, can view PII and can download data
-	users["u2"] = {'name': 'user2', 'email': 'user2@example.com', 'location_restrictions': 'DistrictY',
-	'group': 'Data Manager', 'view_pii': True, 'download_data': True}
+    # user 2: data manager restricted to districtY, can view PII and can download data
+    users["u2"] = {
+        "name": "user2",
+        "email": "user2@example.com",
+        "location_restrictions": "DistrictY",
+        "group": "Data Manager",
+        "view_pii": True,
+        "download_data": True,
+        "supervise_users": True,
+    }
 
-	# user 3: data viewer - no location restrictions, can view PII but cannot download data
-	users["u3"] = {'name': 'user3', 'email': 'user3@example.com',
-	'group': 'Data Viewer', 'view_pii': True, 'download_data': False}
+    # user 3: data viewer - no location restrictions, can view PII but cannot download data
+    users["u3"] = {
+        "name": "user3",
+        "email": "user3@example.com",
+        "group": "Data Viewer",
+        "view_pii": True,
+        "download_data": False,
+        "supervise_users": False,
+    }
 
-	return users
+    return users
--- va_explorer/va_data_management/management/commands/randomize_va_dates.py
+++ va_explorer/va_data_management/management/commands/randomize_va_dates.py
@@ -1,22 +1,24 @@
+import sys
+from math import ceil, sqrt
 from os import environ
-from django.core.management.base import BaseCommand
-from va_explorer.va_data_management.models import VerbalAutopsy
 from random import randint
-from math import sqrt, ceil
-from datetime import date, timedelta
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from va_explorer.va_data_management.models import VerbalAutopsy
 
 # Demo only: script to ramdomize VA dates for demos of e.g. the metrics page
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     help = "Randomize dates for demos, to be run only in development mode"
 
     def handle(self, *args, **options):
-
         if not environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.local":
             message = "This functionality is for demo purposes only in the local environment. Exiting."
             self.stdout.write(self.style.ERROR(message))
-            exit()
+            sys.exit()
 
         # Randomize the dates for all VAs in the system so that they take place over
         # the past ~6 months at (randomness willing) an increasing rate; we want them
@@ -24,10 +26,12 @@ class Command(BaseCommand):
         # dates then assign them
         count = VerbalAutopsy.objects.count()
         days_list = [183 - ceil(sqrt(randint(1, 183 ** 2))) for _ in range(count)]
-        dates_list = [date.today() - timedelta(days=days) for days in days_list]
+        dates_list = [
+            timezone.now() - timezone.timedelta(days=days) for days in days_list
+        ]
         dates_list.sort()
         for va, new_date in zip(VerbalAutopsy.objects.all(), dates_list):
             # Set Id10023 and created and updated all to same date
-            va.Id10023 = va.created = va.updated = va.submissiondate = new_date 
+            va.Id10023 = va.created = va.updated = va.submissiondate = new_date
             va.skip_history_when_saving = True
             va.save()
