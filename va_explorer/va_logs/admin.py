2077ea1693f00d4af2e664bc8b85e5ce34248277
Abraham, Brendan <babraham@mitre.org>
2021-12-10 18:23:35 -0500

    removed extraneous admin and model files

--- va_explorer/va_logs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import VerbalAutopsy, Location, CauseOfDeath
-
-# Register your models here.
-admin.site.register(VerbalAutopsy)
-admin.site.register(Location)
-admin.site.register(CauseOfDeath)
