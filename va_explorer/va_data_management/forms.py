4f2b7f7dd1bdfac7813ac75e194dcec828ed0335
Adam Barnes <ajbarnes@mitre.org>
2022-02-16 22:14:52 -0500

    Add additional radio choices + support checkbox, number, text, readonly, date, time, & datetime

--- va_explorer/va_data_management/forms.py
+++ va_explorer/va_data_management/forms.py
@@ -1,39 +1,56 @@
 from django import forms
-from datetime import datetime
-from .models import VerbalAutopsy
-from .constants import PII_FIELDS
-from va_explorer.va_data_management.utils.date_parsing import parse_date
-from config.settings.base import DATE_FORMATS
 
-from va_explorer.va_data_management.constants import FORM_FIELDS
+from config.settings.base import DATE_FORMATS
+from va_explorer.va_data_management.utils.date_parsing import parse_date
+
+from .constants import PII_FIELDS, FORM_FIELDS, HIDDEN_FIELDS
+from .models import VerbalAutopsy
+
 
 class VerbalAutopsyForm(forms.ModelForm):
-
     class Meta:
         model = VerbalAutopsy
-        exclude = ('id', 'location', 'instanceid')
-        widgets = {
-            }
-        for item in FORM_FIELDS['radio']:
-            widgets[item] = forms.RadioSelect(choices=FORM_FIELDS['radio'][item], attrs={'class':''}) 
-            # because all fields in our model is textfield, we are overriding some of the field's field type to radio field here
+        exclude = HIDDEN_FIELDS
+        widgets = {}
+        # Because (the massive amount of) model fields are textarea by default,
+        # we are overriding the display logic via here + mappings in .constants
+        for form_field in FORM_FIELDS["text"]:
+            widgets[form_field] = forms.TextInput()
+        for form_field in FORM_FIELDS["radio"]:
+            widgets[form_field] = forms.RadioSelect(
+                choices=FORM_FIELDS["radio"][form_field], attrs={"class": "va-check"}
+            )
+        for form_field in FORM_FIELDS["checkbox"]:
+            widgets[form_field] = forms.CheckboxSelectMultiple(
+                choices=FORM_FIELDS["checkbox"][form_field], attrs={"class": "va-check"}
+            )
+        for form_field in FORM_FIELDS["number"]:
+            widgets[form_field] = forms.NumberInput()
+        for form_field in FORM_FIELDS["date"]:
+            widgets[form_field] = forms.DateInput()
+        for form_field in FORM_FIELDS["time"]:
+            widgets[form_field] = forms.TimeInput()
+        for form_field in FORM_FIELDS["datetime"]:
+            widgets[form_field] = forms.DateTimeInput()
+        for form_field in FORM_FIELDS["display"]:
+            widgets[form_field] = forms.TextInput(attrs={'readonly': 'readonly'})
+
+
 
     def __init__(self, *args, **kwargs):
-        include_pii = kwargs.pop('include_pii', True)
+        include_pii = kwargs.pop("include_pii", True)
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-             # this only works assuming the two field types we have are radio and textfields
-             # this if statements makes changes to all textfield inputs we have by checking to see if that field isn't in our radiochoices
-            if field_name not in FORM_FIELDS['radio']:
-                field.widget.attrs['rows'] = '1' 
-                if field.widget.attrs.get('class'):
-                    field.widget.attrs['class'] += 'form-control'
-                else:
-                    field.widget.attrs['class']='form-control'
+        # Handle text/textarea input types
+        for _, field in self.fields.items():
+            if not field.widget.attrs.get("class"):
+                field.widget.attrs["class"] = "form-control"
+            if isinstance(field.widget, forms.Textarea):
+                field.widget.attrs["rows"] = "3"
+
         if not include_pii:
             for field in PII_FIELDS:
                 del self.fields[field]
-    
+
     # TODO: to display the error msgs properly, we need to use crispy forms in the template
     # for now we will just display the errors at the top of the page
     def clean(self, *args, **kwargs):
@@ -43,11 +60,10 @@ class VerbalAutopsyForm(forms.ModelForm):
         cleaned_data = super(VerbalAutopsyForm, self).clean(*args, **kwargs)
 
         if "Id10023" in cleaned_data:
-            validate_date_format(
-                self, cleaned_data["Id10023"]
-            )
+            validate_date_format(self, cleaned_data["Id10023"])
 
         return cleaned_data
+
 
 def validate_date_format(form, Id10023):
     """
@@ -55,9 +71,12 @@ def validate_date_format(form, Id10023):
     """
     # TODO add a date picker to the form so we don't have to check the string format
     if Id10023 != "dk":
-        try: 
+        try:
             parse_date(Id10023, strict=True)
         except ValueError:
             form._errors["Id10023"] = form.error_class(
-                [f"Field Id10023 must be in \"dk\" if unknown or in one of following date formats: {list(DATE_FORMATS.values())}"]
+                [
+                    f'Field Id10023 must be "DK" if unknown or in one of \
+                      following date formats: {list(DATE_FORMATS.values())}'
+                ]
             )
