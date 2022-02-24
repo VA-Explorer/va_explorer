64bf790bcb07a0c84b49750724d1828cd7093077
Diana Eastman <diana.eastman@gmail.com>
2022-02-24 16:48:00 -0500

    VA Explorer 148: Support a deletion workflow for duplicate VAs (#208)
    * Add new fields in VerbalAutopsy model for supporting marking of duplicates and soft deletes. Add methods to VerbalAutopsy model for these purposes as well. Add initial tests.
    
    * Add va_data_cleanup app.
    
    * Adjust signals due to recursion issues.
    
    * Add SoftDeletionModel, SoftDeletionManager, and SoftDeletionQuerySet.
    
    * Update relevant templates.
    
    * Give admin users the relevant permissions.
    
    * Add va_data_cleanup app.
    
    * Remove select fields from edit form.
    
    * Make VerbalAutopsy a SoftDeletionModel; continue to update methods related to marking duplicates.
    
    * Add relevant urls.
    
    * Remove breakpoint from tests.
    
    * Add print statements to loading.py.
    
    * Add delete view for deleting individual VAs.
    
    * Consolidate added fields into one migration file.
    
    * Adjust save logic in model, update tests, and add download functionality.
    
    * Add configuration point for QUESTIONS_TO_AUTODETECT_DUPLICATES.
    
    * Remove unused files and minimize unrelated changes.
    
    * Adjust migrations.
    
    * Remove erroneous comma.
    
    * Add instanceid back into excluded element in form."
    
    * Update duplicate count.
    
    * Update check for migration within mark_vas_as_duplicate command.
    
    * Fix migrations.
    
    * Remove commented out import.
    
    * Remove unique_identifiers and move questions_to_autodetect_duplicates.
    
    * Clean up diff.
    
    * Remove set_trace().
    
    * Add Managers for models related to Verbal Autopsy, clean up tests, add more comments, add tests for va_data_cleanup.
    
    * Update test.
    
    * Update test.
    
    * Clean up CSS.
    
    * Add more tests.
    
    * Add updated factories.
    
    * Add duplicate count to Home and Data Management Pages.
    
    * Update README with instructions about management of duplicate VAs.
    
    * Add duplicate stats to dashboard.
    
    * Add additional line to the README.
    
    * Update language from auto-detected to potential.
    
    * Missed a couple instances of auto-detected --> flagged as potential in language change.
    
    * Update Data Manager permissions.
    
    * Update language in view test to match changed language (auto-detect -> flag as potential duplicate.
    
    * Remove unnecessary numbering from tests.
    
    * Update context object to align with the partial it is populating.
    
    * Add extension to filename.
    
    Co-authored-by: Diana Eastman <deastman@mitre.org>

---
+++ va_explorer/va_data_cleanup/tests/test_views.py
@@ -0,0 +1,159 @@
+import pytest
+from django.test import Client
+from django.contrib.auth.models import Permission
+from va_explorer.users.models import User
+from va_explorer.tests.factories import GroupFactory, VerbalAutopsyFactory, UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def questions_to_autodetect_duplicates():
+    return "Id10017, Id10010"
+
+
+@pytest.fixture
+def view_datacleanup_group():
+    can_view_datacleanup = Permission.objects.filter(codename="view_datacleanup").first()
+    return GroupFactory.create(permissions=[can_view_datacleanup ])
+
+
+@pytest.fixture
+def download_group():
+    can_download = Permission.objects.filter(codename="download").first()
+    return GroupFactory.create(permissions=[can_download])
+
+
+@pytest.fixture
+def bulk_download_group():
+    can_bulk_download = Permission.objects.filter(codename="bulk_download").first()
+    return GroupFactory.create(permissions=[can_bulk_download])
+
+
+# View the index page
+def test_index_with_valid_permission(user: User, settings, view_datacleanup_group, questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get("/va_data_cleanup/")
+
+    assert response.status_code == 200
+
+
+# A user must have the view_datacleanup permission to view the index
+def test_index_with_invalid_permission(user: User, settings, questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create()
+
+    client = Client()
+    client.force_login(user=user)
+
+    response = client.get("/va_data_cleanup/")
+    assert response.status_code == 403
+
+
+def test_index_with_valid_permission_and_invalid_configuration(user: User, settings, view_datacleanup_group):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = None
+
+    user = UserFactory.create(groups=[view_datacleanup_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    response = client.get("/va_data_cleanup/")
+    assert response.status_code == 200
+    assert bytes("Your system is currently not configured to automatically flag Verbal Autopsies as potential duplicates.",
+                 "utf-8") in response.content
+
+
+def test_download_with_valid_permission(user: User, settings, view_datacleanup_group, download_group,
+                                        questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group, download_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    va = VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get(f'/va_data_cleanup/download/{va.id}')
+
+    assert response.status_code == 200
+    assert response._headers["content-type"][1] == "text/csv"
+
+
+def test_download_with_invalid_permission(user: User, settings, view_datacleanup_group,
+                                        questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    va = VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get(f'/va_data_cleanup/download/{va.id}')
+
+    assert response.status_code == 403
+    assert response._headers["content-type"][1] == "text/html; charset=utf-8"
+
+
+# Test if a user arbitrarily passes in a non-duplicate VA, 403 raised
+def test_download_with_valid_permission_non_duplicate_va(user: User, settings, view_datacleanup_group,
+                                        questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    va = VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get(f'/va_data_cleanup/download/{va.id}')
+
+    assert response.status_code == 403
+    assert response._headers["content-type"][1] == "text/html; charset=utf-8"
+
+
+def test_bulk_download_with_valid_permission(user: User, settings, view_datacleanup_group, bulk_download_group,
+                                        questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group, bulk_download_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get("/va_data_cleanup/download_all")
+
+    assert response.status_code == 200
+    assert response._headers["content-type"][1] == "text/csv"
+
+
+def test_bulk_download_with_invalid_permission(user: User, settings, view_datacleanup_group,
+                                        questions_to_autodetect_duplicates):
+    settings.QUESTIONS_TO_AUTODETECT_DUPLICATES = questions_to_autodetect_duplicates
+    user = UserFactory.create(groups=[view_datacleanup_group])
+
+    client = Client()
+    client.force_login(user=user)
+
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+    VerbalAutopsyFactory.create(Id10017='Victim name', Id10010='Interviewer name')
+
+    response = client.get("/va_data_cleanup/download_all")
+
+    assert response.status_code == 403
+    assert response._headers["content-type"][1] == "text/html; charset=utf-8"
+
---
+++ va_explorer/va_data_management/management/commands/mark_vas_as_duplicate.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+
+from django.conf import settings
+from va_explorer.va_data_management.models import VerbalAutopsy
+
+
+class Command(BaseCommand):
+    help = "Marks existing VAs as duplicate"
+
+
+    def handle(self, *args, **options):
+        if not settings.QUESTIONS_TO_AUTODETECT_DUPLICATES:
+            self.stdout.write(
+                self.style.ERROR(
+                    "Error: Configuration, QUESTIONS_TO_AUTODETECT_DUPLICATES, is an empty list. \n"
+                    "Please update you .env file with a list of questions by Id that will be used to autodetect "
+                    "duplicates."
+                )
+            )
+            exit()
+
+        try: 
+            all_existing_vas = list(VerbalAutopsy.objects.all())
+        except:
+            self.stdout.write(
+                self.style.ERROR(
+                    "Missing required database fields in Verbal Autopsy model to mark Verbal Autopsies as duplicate."
+                    "Please run latest migration to add fields."
+                )
+            )
+            exit()
+
+        self.stdout.write(self.style.SUCCESS("Generating unique identifiers..."))
+        for va in all_existing_vas:
+            va.generate_unique_identifier_hash()
+
+        self.stdout.write(self.style.SUCCESS("Unique identifiers generated!"))
+        VerbalAutopsy.objects.bulk_update(all_existing_vas, ["unique_va_identifier"])
+
+        self.stdout.write(self.style.SUCCESS("Marking existing VAs as duplicate..."))
+        VerbalAutopsy.mark_duplicates()
+
+        duplicate_count = VerbalAutopsy.objects.filter(duplicate=True).count()
+        self.stdout.write(self.style.SUCCESS(f'Successfully marked {duplicate_count} existing VAs as duplicate!'))
