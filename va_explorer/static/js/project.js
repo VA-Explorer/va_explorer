cc1756985377d919bda7d285026b6f9366b4082e
Tyler Manderfield <jmanderfield@mitre.org>
2022-03-18 13:04:49 -0400

    cleanup of comments

--- va_explorer/static/js/project.js
+++ va_explorer/static/js/project.js
@@ -3,11 +3,7 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 $('#hidden-check').change(function() {
-  var vis = this.checked ? 'visible' : 'hidden';
   var vis = this.checked ? 'none' : '';
-  console.log("CONSOLEEEE");
-  // $('.nan').css('visibility', vis);
-  // $('td[class=""]').css('visibility',vis);
   $('.nan').css('display', vis);
   $('td[class=""]').css('display',vis);
 })
