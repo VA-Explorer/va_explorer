ef11486d18ecda99f7fa14e8fffc6737e17445c7
Diana Eastman <deastman@mitre.org>
2022-04-26 15:45:11 -0400

    Update tests.

--- va_explorer/va_export/tests/test_views.py
+++ va_explorer/va_export/tests/test_views.py
@@ -1,12 +1,10 @@
+import io
 import json
-from io import BytesIO
-from urllib.parse import urlencode
+import zipfile
 
-import pandas as pd
 import pytest
 from django.contrib.auth.models import Permission
-from django.test import RequestFactory
-from django.urls import reverse
+from django.test import Client, RequestFactory
 
 from va_explorer.tests.factories import (
     GroupFactory,
@@ -18,13 +16,18 @@ from va_explorer.users.models import User
 from va_explorer.va_data_management.constants import REDACTED_STRING
 from va_explorer.va_data_management.models import CauseOfDeath, Location, VerbalAutopsy
 from va_explorer.va_export.forms import VADownloadForm
-from va_explorer.va_export.views import va_api_view
 
 pytestmark = pytest.mark.django_db
 
+CSV_ZIP_FILE_NAME = 'export.csv.zip'
+JSON_ZIP_FILE_NAME = 'export.json.zip'
+CSV_FILE_NAME = 'va_download.csv'
+JSON_FILE_NAME = 'va_download.json'
+FILE_CONTENT_TYPE = 'application/zip'
+POST_URL = "/va_export/verbalautopsy/"
+
 
 def build_test_db():
-
     # Build locations
     province = LocationFactory.create()
     district1 = province.add_child(name="District1", location_type="district")
@@ -59,169 +62,209 @@ def build_test_db():
 
 
 class TestAPIView:
-    def test_csv_download(self, rf: RequestFactory):
+    def test_csv_download(self, user: User):
         build_test_db()
-        request = rf.get("/va_export/verbalautopsy/?format=csv")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
 
+        c = Client()
+        c.force_login(user=user)
+
+        response = c.post(POST_URL, data={'format': 'csv'})
         assert response.status_code == 200
-        # confirm response content has some data
-        assert len(response.content) > 0
-        df = pd.read_csv(BytesIO(response.content))
-        # confirm that all data was downloaded
-        db_va_ct = VerbalAutopsy.objects.count()
-        download_ct = df.shape[0]
-        assert download_ct == db_va_ct
 
-    def test_json_download(self, rf: RequestFactory):
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={CSV_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 5
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_json_download(self, user: User):
         build_test_db()
-        request = rf.get("/va_export/verbalautopsy/?format=json")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
 
+        c = Client()
+        c.force_login(user=user)
+
+        response = c.post(POST_URL, data={'format': 'json'})
         assert response.status_code == 200
-        # confirm response content has some data
-        assert len(response.content) > 0
 
-        json_data = json.loads(response.content)
-        # confirm that all data was downloaded
-        db_va_ct = VerbalAutopsy.objects.count()
-        # NOTE: assumes records are stored in a wrapper with 'count' and 'record' keys.
-        # If this structure changes, need to update this test
-        download_ct = json_data["count"]
-        assert download_ct == db_va_ct
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={JSON_ZIP_FILE_NAME}'
 
-    def test_download_csv_with_no_matching_vas(self, rf: RequestFactory):
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+
+            json_data = json.loads(zipped_file.read(JSON_FILE_NAME))
+            assert json_data["count"] == 4
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_download_csv_with_no_matching_vas(self, user: User):
         build_test_db()
         # only download data from "No VA Facility", which will have no matching VAs
         no_va_facility = Location.objects.get(name="No VA Facility")
 
-        request = rf.get(
-            f"/va_export/verbalautopsy/?format=csv&locations={no_va_facility.pk}"
-        )
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
+        c = Client()
+        c.force_login(user=user)
 
+        response = c.post(POST_URL, data={'format': 'csv', 'locations': no_va_facility})
         assert response.status_code == 200
-        # Assert content consists only of line break
-        assert response.content.decode("utf-8") == "\n"
 
-    def test_download_json_with_no_matching_vas(self, rf: RequestFactory):
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={CSV_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # The single line is the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 1
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_download_json_with_no_matching_vas(self, user: User):
         build_test_db()
         # only download data from "No VA Facility", which will have no matching VAs
         no_va_facility = Location.objects.get(name="No VA Facility")
 
-        request = rf.get(
-            f"/va_export/verbalautopsy/?format=json&locations={no_va_facility.pk}"
-        )
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
+        c = Client()
+        c.force_login(user=user)
 
+        response = c.post(POST_URL, data={'format': 'json', 'locations': no_va_facility})
         assert response.status_code == 200
-        # Assert content consists of the correct object
-        assert response.content.decode("utf-8") == '{"count": 0, "records": "[]"}'
 
-    def test_location_filtering(self, rf: RequestFactory):
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={JSON_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+
+            json_data = json.loads(zipped_file.read(JSON_FILE_NAME))
+            assert json_data["count"] == 0
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_location_filtering(self, user: User):
         build_test_db()
         # only download data from location a
-        loc_a = Location.objects.get(name="Facility1")
+        facility_1 = Location.objects.get(name="Facility1")
 
-        request = rf.get(f"/va_export/verbalautopsy/?format=csv&locations={loc_a.pk}")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
+        c = Client()
+        c.force_login(user=user)
 
+        response = c.post(POST_URL, data={'format': 'csv', 'locations': facility_1})
         assert response.status_code == 200
-        # confirm response content has some data
-        assert len(response.content) > 0
-        # confirm only 2/4 vas were downloaded
-        df = pd.read_csv(BytesIO(response.content))
-        download_ct = df.shape[0]
-        db_ct = VerbalAutopsy.objects.filter(location__id=loc_a.pk).count()
-        assert download_ct == db_ct
-        # confirm only from location a
-        uniq_loc = df["location"].unique().tolist()
-        assert (len(uniq_loc) == 1) and (uniq_loc[0] == loc_a.name)
 
-    def test_cod_filtering(self, rf: RequestFactory):
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={CSV_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 3
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_cod_filtering(self, user: User):
         build_test_db()
         # only download data from location a
         cod_name = "cod_a"
 
-        request = rf.get(f"/va_export/verbalautopsy/?format=csv&causes={cod_name}")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
+        c = Client()
+        c.force_login(user=user)
 
+        response = c.post(POST_URL, data={'format': 'csv', 'causes': cod_name})
         assert response.status_code == 200
-        # confirm response content has some data
-        assert len(response.content) > 0
-        # confirm the right number of VAs were downloaded
-        df = pd.read_csv(BytesIO(response.content))
-        download_ct = df.shape[0]
-        db_ct = VerbalAutopsy.objects.filter(causes__cause=cod_name).count()
-        assert download_ct == db_ct
-        # confirm all VAs have cause cod_a
-        uniq_cod = df["cause"].unique().tolist()
-        assert (len(uniq_cod) == 1) and (uniq_cod[0] == cod_name)
 
-    def test_time_filter(self, rf: RequestFactory):
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={CSV_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 3
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_time_filter(self, user: User):
         build_test_db()
         # only download data from location a
         start_date, end_date = "2020-01-01", "2021-01-01"
 
-        request = rf.get(
-            f"/va_export/verbalautopsy/?format=csv&start_date={start_date}&end_date={end_date}"
-        )
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
+        c = Client()
+        c.force_login(user=user)
 
+        response = c.post(POST_URL, data={'format': 'csv', 'start_date': start_date, 'end_date': end_date})
         assert response.status_code == 200
-        # confirm response content has some data
-        assert len(response.content) > 0
-        df = pd.read_csv(BytesIO(response.content))
-        # confirm only 1 VA was downloaded
-        download_ct = df.shape[0]
-        # assumes filtering is done by date of death (Id10023). If that changes, need to update fields below
-        db_ct = VerbalAutopsy.objects.filter(
-            Id10023__gte=start_date, Id10023__lte=end_date
-        ).count()
-        assert download_ct == db_ct
-        # confirm all VAs are within chosen time period
-        assert (
-            (df["Id10023"] >= start_date) & (df["Id10023"] <= end_date)
-        ).sum() == db_ct
 
-    def test_combined_filter_csv(self, rf: RequestFactory):
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 2
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_combined_filter_csv(self, user: User):
         build_test_db()
         # 1. Download from facility A after 1/1/2020 with COD_a in CSV format. Assert only VA 4 is downloaded
         start_date = "2020-01-01"
         loc_a = Location.objects.get(name="Facility1")
         cod_name = "cod_a"
-        comb_csv_query = (
-            f"format=csv&start_date={start_date}&locations={loc_a.pk}&causes={cod_name}"
-        )
-        request = rf.get(f"/va_export/verbalautopsy/?{comb_csv_query}")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
 
+        query_data = {
+            'format': 'csv',
+            'start_date': start_date,
+            'locations': loc_a.pk,
+            'causes': cod_name
+        }
+
+        c = Client()
+        c.force_login(user=user)
+
+        response = c.post(POST_URL, data=query_data)
         assert response.status_code == 200
+
         # confirm correct number of VAs downloaded
         db_ct = VerbalAutopsy.objects.filter(
             Id10023__gte=start_date, causes__cause=cod_name, location__id=loc_a.pk
         ).count()
-        df = pd.read_csv(BytesIO(response.content))
-        download_ct = df.shape[0]
-        assert download_ct == db_ct
-        # confirm all VAs are within chosen time period
-        assert (df["Id10023"] >= start_date).sum() == db_ct
-        # confirm all VAs have cause cod_a
-        uniq_cod = df["cause"].unique().tolist()
-        assert (len(uniq_cod) == 1) and (uniq_cod[0] == cod_name)
-        # confirm all VAs are from location a
-        uniq_loc = df["location"].unique().tolist()
-        assert (len(uniq_loc) == 1) and (uniq_loc[0] == loc_a.name)
 
-    def test_combined_filter_json(self, rf: RequestFactory):
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == db_ct + 1
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_combined_filter_json(self, user: User):
         build_test_db()
         # 2. Download data from facility A from before 2020 with COD b in JSON format. Assert only VA 1 is downloaded
         # NOTE: assumes records are stored in a wrapper with 'count' and 'record' keys.
@@ -230,45 +273,63 @@ class TestAPIView:
         end_date = "2020-01-01"
         loc_a = Location.objects.get(name="Facility1")
         cod_name = "cod_b"
-        comb_csv_query = (
-            f"format=json&end_date={end_date}&locations={loc_a.pk}&causes={cod_name}"
-        )
-        request = rf.get(f"/va_export/verbalautopsy/?{comb_csv_query}")
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
 
-        assert response.status_code == 200
+        query_data = {
+            'format': 'json',
+            'end_date':  end_date,
+            'locations': loc_a.pk,
+            'causes': cod_name
+        }
+
+        c = Client()
+        c.force_login(user=user)
+
         # confirm correct number of VAs downloaded
         db_ct = VerbalAutopsy.objects.filter(
             Id10023__lte=end_date, causes__cause=cod_name, location__id=loc_a.pk
         ).count()
-        json_obj = json.loads(response.content)
-        download_ct = json_obj["count"]
-        assert download_ct == db_ct
-        # confirm all VAs are within chosen time period
-        df = pd.read_json(json_obj["records"])
-        assert (df["Id10023"] <= end_date).sum() == db_ct
-        # confirm all VAs have cause cod_a
-        uniq_cod = df["cause"].unique().tolist()
-        assert (len(uniq_cod) == 1) and (uniq_cod[0] == cod_name)
-        # confirm all VAs are from location a
-        uniq_loc = df["location"].unique().tolist()
-        assert (len(uniq_loc) == 1) and (uniq_loc[0] == loc_a.name)
+
+        response = c.post(POST_URL, data=query_data)
+        assert response.status_code == 200
+
+        # Django 3.2 has response.headers. For now, we'll access them per below
+        # See https://docs.djangoproject.com/en/4.0/ref/request-response/#django.http.HttpResponse.headers
+        assert response._headers['content-type'][1] == FILE_CONTENT_TYPE
+        assert response._headers['content-disposition'][1] == f'attachment; filename={JSON_ZIP_FILE_NAME}'
+
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+
+            json_data = json.loads(zipped_file.read(JSON_FILE_NAME))
+            assert json_data["count"] == db_ct
+        finally:
+            zipped_file.close()
+            f.close()
+
+        assert response.status_code == 200
 
     def test_redacted_download(self, rf: RequestFactory):
         build_test_db()
-        request = rf.get("/va_export/verbalautopsy/?format=csv")
-        request.user = User.objects.get(name="no_pii")
-        response = va_api_view(request)
+        user = User.objects.get(name="no_pii")
 
+        c = Client()
+        c.force_login(user=user)
+
+        response = c.post(POST_URL, data={'format': 'csv'})
         assert response.status_code == 200
-        # confirm redacted string appearing in records
-        assert REDACTED_STRING in str(response.content)
-        # confirm all records are included in download
-        df = pd.read_csv(BytesIO(response.content))
-        assert VerbalAutopsy.objects.count() == df.shape[0]
 
-    def test_download_via_form(self, rf: RequestFactory):
+        try:
+            f = io.BytesIO(response.content)
+            zipped_file = zipfile.ZipFile(f, 'r')
+            # Add one for the variable name header in the csv
+            assert len(zipped_file.open(CSV_FILE_NAME).readlines()) == 5
+            assert REDACTED_STRING in str(zipped_file.read(CSV_FILE_NAME))
+        finally:
+            zipped_file.close()
+            f.close()
+
+    def test_download_via_form(self, user: User):
         build_test_db()
         # filter by id of last location in test db
         loc_id = Location.objects.last().pk
@@ -282,12 +343,3 @@ class TestAPIView:
         )
 
         assert download_form.is_valid()
-
-        form_data = download_form.cleaned_data
-        api_url = reverse("va_export:va_api") + "?" + urlencode(form_data)
-        request = rf.get(api_url)
-        request.user = User.objects.get(name="admin")
-        response = va_api_view(request)
-        df = pd.read_csv(BytesIO(response.content))
-        # result should be a subset of all data
-        assert df.shape[0] < VerbalAutopsy.objects.count()
