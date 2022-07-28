7da2ed2de228c369e137b87f54ca306e822723b6
Diana Eastman <deastman@mitre.org>
2022-07-28 17:09:52 -0400

    Edit the GitHub links.

--- docs/conf.py
+++ docs/conf.py
@@ -263,5 +263,5 @@ html_context['downloads'].append(('epub',
 
 html_context['display_github'] = True
 html_context['github_user'] = 'va_explorer'
-html_context['github_repo'] = 'va_explorer'
-html_context['github_version'] = 'master/docs/'
+html_context['github_repo'] = 'VA-Explorer'
+html_context['github_version'] = 'tree/docs/'
