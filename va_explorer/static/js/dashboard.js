09b154fa3e1d6a450d53cdc95c3a80e6e4f416ac
Harout Boujakjian <haroutb719@gmail.com>
2022-08-01 14:36:00 -0400

    add simple leaflet map with zambia geojson svg layer that can switch between district and province views

--- va_explorer/static/js/dashboard.js
+++ va_explorer/static/js/dashboard.js
@@ -1,4 +1,4 @@
-Vue.use('stacked-bar-chart', 'line-chart')
+Vue.use('stacked-bar-chart', 'line-chart', 'loader-spinning')
 
 const dashboard = new Vue({
     el: '#dashboardApp',
@@ -9,8 +9,11 @@ const dashboard = new Vue({
 
             rawdata: [],
             invalid: "",
-            location_types: [],
+            location_types: ["Province", "District"],
             locations: [],
+            map: null,
+            geojson: {},
+            layer: null,
 
             demographicsHeight: 0,
             demographicsWidth: 0,
@@ -20,16 +23,12 @@ const dashboard = new Vue({
             topCausesLimit: 50,
             startDate: "",
             causeSelected: "",
+            borderType: "District"
         }
     },
     computed: {
         listOfCauses() {
             return [...new Set(this.rawdata.map(item => item.cause))].sort()
-        },
-        listOfDistrictAndProvinces() {
-            const provinces = [...new Set(this.rawdata.map(item => item.province))]
-            const districts = [...new Set(this.rawdata.map(item => item.district))]
-            return [...provinces, ...districts].sort()
         },
         filteredData() {
             return this.rawdata
@@ -49,9 +48,9 @@ const dashboard = new Vue({
             }
         },
         geographicSums() {
-            let geo_groups = d3.rollups(this.filteredData, v => v.length, d => d[state.borders])
+            let geo_groups = d3.rollups(this.filteredData, v => v.length, d => d[this.borderType.toLowerCase()])
             let map = Object.fromEntries(geo_groups)
-            return Object.keys(map).map(item => ({geography: item, value: map[item]}))
+            return map
         },
         gender() {
             let groupby_gender_age = d3.rollups(this.filteredData,
@@ -85,11 +84,13 @@ const dashboard = new Vue({
             return Object.keys(map)
                 .map(item => ({date: item, value: map[item]}))
                 .sort((a, b) => new Date(a.date) - new Date(b.date))
-        }
+        },
     },
     async created() {
+        /*
+        Request data from API endpoint
+         */
         this.csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
         const res = await fetch('http://localhost:8000/va_analytics/api/dashboard', {
             method: 'GET',
             headers: {'X-CSRFToken': this.csrftoken, 'Content-Type': 'application/json'},
@@ -99,24 +100,59 @@ const dashboard = new Vue({
         const jsonRes = await res.json()
         this.rawdata = jsonRes.data.valid.map(item => ({...item, active: true}))
         this.invalid = jsonRes.data.invalid
-        this.location_types = jsonRes.location_types
         this.locations = jsonRes.locations
-        console.log(jsonRes)
+
+        // Request geojson data
+        const url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/static/`
+        const geojsonRes = await fetch(`${url}/data/zambia_geojson.json`)
+        this.geojson = await geojsonRes.json()
     },
-    mounted() {
+    async mounted() {
         this.resizeCharts()
         window.addEventListener('resize', this.resizeCharts)
+
+        await this.initializeBaseMap()
     },
     beforeDestroy() {
+        // necessary to remove resize listener to avoid memory leak after switching to different view
         window.removeEventListener('resize', this.resizeCharts)
     },
     methods: {
+        async initializeBaseMap() {
+            this.map = L.map('map').setView([-13, 27], 6)
+
+            this.map.attributionControl.setPrefix('')
+
+            const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 10,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map)
+        },
+        addGeoJSONToMap() {
+           if (this.layer) {
+                this.map.removeLayer(this.layer)
+            }
+
+            const borders = ['Country', this.borderType]
+            let geojson = JSON.parse(JSON.stringify(this.geojson))
+            geojson.features = this.geojson.features.filter(feature => {
+                return borders.includes(feature.properties.area_level_label)
+            })
+            this.layer = L.geoJson(geojson, {
+                style: function (feature) {
+                    return {opacity: 0.9, weight: 1, dashArray: '2', color: 'red'}
+                }
+            }).addTo(this.map)
+        },
         getMonth(date) {
             const month = date.getUTCMonth()
             const year = date.getUTCFullYear()
             return new Date(year, month, 0)
         },
         resizeCharts() {
+            /*
+            Apply method on mounted and with event listener to automatically resize charts
+             */
             this.demographicsWidth = this.$refs.demographics.clientWidth - 1
             this.demographicsHeight = this.$refs.demographics.clientHeight - 1
 
@@ -137,6 +173,14 @@ const dashboard = new Vue({
         },
         filterByDate(startDate) {
             this.rawdata = this.rawdata.map(item => ({...item, active: new Date(startDate) < new Date(item.date)}))
+        },
+    },
+    watch: {
+        geojson() {
+            this.addGeoJSONToMap()
+        },
+        borderType() {
+            this.addGeoJSONToMap()
         },
     }
 })
