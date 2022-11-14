b55c84abc05b85131b51df308dcc85d57a68625d
Harout Boujakjian <haroutb719@gmail.com>
2022-11-14 11:20:50 -0500

    FIX: add CSRF_TRUSTED_ORIGIN to include scheme and hostname which is now required in Django 4.0

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,6 +11,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default=get_random_secret_key())
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["va_explorer.org"])
 
+CSRF_TRUSTED_ORIGINS = ["https://*.moh.gov.zm"]
+
 # Caches
 
 CACHES = {
